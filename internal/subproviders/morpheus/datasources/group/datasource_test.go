38b6093076bb3210d9661b73ec24f00e4479018d
Jason Quigley <jason.quigley@hpe.com>
2025-05-21 15:16:54 +0100

    Add testing group creation via API

--- internal/subproviders/morpheus/datasources/group/datasource_test.go
+++ internal/subproviders/morpheus/datasources/group/datasource_test.go
@@ -1,15 +1,21 @@
 package group_test
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
 	"regexp"
 	"testing"
 
+	"github.com/HewlettPackard/hpe-morpheus-go-sdk/sdk"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
 	"github.com/HPE/terraform-provider-hpe/internal/provider"
 	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus"
+	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/clientfactory"
 	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/datasources/group/consts"
 )
 
@@ -39,6 +45,61 @@ provider "hpe" {
 }
 `
 
+var group sdk.ListGroups200ResponseAllOfGroupsInner
+
+func newClient(t *testing.T, ctx context.Context) *sdk.APIClient {
+	t.Helper()
+
+	return clientfactory.NewAPIClient(
+		ctx,
+		os.Getenv("TF_VAR_testacc_morpheus_url"),
+		os.Getenv("TF_VAR_testacc_morpheus_username"),
+		os.Getenv("TF_VAR_testacc_morpheus_password"),
+		"",
+		clientfactory.WithInsecureTLS())
+}
+
+func createGroup(t *testing.T) sdk.ListGroups200ResponseAllOfGroupsInner {
+	t.Helper()
+
+	ctx := context.TODO()
+
+	addGroup := sdk.NewAddGroupsRequestGroupWithDefaults()
+	addGroup.SetName("testacc-" + t.Name())
+	addGroup.SetCode("jason")
+	addGroup.SetLocation("here")
+
+	addGroupReq := sdk.NewAddGroupsRequest(*addGroup)
+
+	client := newClient(t, ctx)
+
+	g, hresp, err := client.GroupsAPI.AddGroups(ctx).AddGroupsRequest(*addGroupReq).Execute()
+	if err != nil || hresp.StatusCode != http.StatusOK {
+		t.Fatalf("POST failed for group %d - %v", 1, err)
+	}
+
+	group := g.GetGroup()
+
+	t.Cleanup(func() {
+		deleteGroup(t, group.GetId())
+	})
+
+	return group
+}
+
+func deleteGroup(t *testing.T, id int64) {
+	t.Helper()
+
+	ctx := context.TODO()
+
+	client := newClient(t, ctx)
+
+	_, hresp, err := client.GroupsAPI.RemoveGroups(ctx, id).Execute()
+	if err != nil || hresp.StatusCode != http.StatusOK {
+		t.Fatalf("POST failed for group %d - %v", 1, err)
+	}
+}
+
 func newProviderWithError() (tfprotov6.ProviderServer, error) {
 	providerInstance := provider.New("test", morpheus.New())()
 
@@ -54,21 +115,25 @@ func TestAccMorpheusFindGroupById(t *testing.T) {
 		t.Skip("Skipping slow test in short mode")
 	}
 
+	group := createGroup(t)
+	groupID := fmt.Sprintf("%d", group.GetId())
+	groupName := group.GetName()
+
 	config := providerConfig + `
       data "hpe_morpheus_group" "test" {
-        id = 1
+        id = ` + groupID + `
       }`
 
 	checks := []resource.TestCheckFunc{
 		resource.TestCheckResourceAttr(
 			"data.hpe_morpheus_group.test",
 			"name",
-			"Jason",
+			groupName,
 		),
 		resource.TestCheckResourceAttr(
 			"data.hpe_morpheus_group.test",
 			"id",
-			"1",
+			groupID,
 		),
 	}
 
@@ -90,21 +155,25 @@ func TestAccMorpheusFindGroupByName(t *testing.T) {
 		t.Skip("Skipping slow test in short mode")
 	}
 
+	group := createGroup(t)
+	groupID := fmt.Sprintf("%d", group.GetId())
+	groupName := group.GetName()
+
 	config := providerConfig + `
       data "hpe_morpheus_group" "test" {
-        name = "Jason" 
+        name = "` + groupName + `" 
       }`
 
 	checks := []resource.TestCheckFunc{
 		resource.TestCheckResourceAttr(
 			"data.hpe_morpheus_group.test",
 			"name",
-			"Jason",
+			groupName,
 		),
 		resource.TestCheckResourceAttr(
 			"data.hpe_morpheus_group.test",
 			"id",
-			"1",
+			groupID,
 		),
 	}
 
