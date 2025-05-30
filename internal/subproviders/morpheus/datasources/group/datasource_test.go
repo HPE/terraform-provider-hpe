4a5ac9f96cc72be5e1729a149283ba6bef8c423d
Jason Quigley <jason.quigley@hpe.com>
2025-05-30 14:09:32 +0100

    remove change to group

--- internal/subproviders/morpheus/datasources/group/datasource_test.go
+++ internal/subproviders/morpheus/datasources/group/datasource_test.go
@@ -65,7 +65,7 @@ func TestAccMorpheusFindGroupById(t *testing.T) {
 	groupID := fmt.Sprintf("%d", group.GetId())
 	groupName := group.GetName()
 
-	config := testhelpers.RenderExample(t, "example-id.hcl", `id\s*=.*`, `id = `+groupID)
+	config := testhelpers.ReadExample(t, "example-id.hcl", `id\s*=.*`, `id = `+groupID)
 
 	checks := []resource.TestCheckFunc{
 		resource.TestCheckResourceAttr(
@@ -107,7 +107,7 @@ func TestAccMorpheusFindGroupByName(t *testing.T) {
 	groupID := fmt.Sprintf("%d", group.GetId())
 	groupName := group.GetName()
 
-	config := testhelpers.RenderExample(t, "example-name.hcl", `name\s*=.*`, `name = "`+groupName+`"`)
+	config := testhelpers.ReadExample(t, "example-name.hcl", `name\s*=.*`, `name = "`+groupName+`"`)
 
 	checks := []resource.TestCheckFunc{
 		resource.TestCheckResourceAttr(
