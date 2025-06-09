e39566c69df6abed8516393e7d276ce1eadc5d71
naoise-osullivan-hpe <naoise.osullivan@hpe.com>
2025-06-09 11:01:06 +0000

    fix(consts):consts moved to datasource.go

--- internal/subproviders/morpheus/datasources/environment/consts/consts.go
+++
@@ -1,8 +0,0 @@
-package consts
-
-const (
-	ErrorNoValidSearchTerms   = `no valid search terms - an id or name is required`
-	ErrorRunningPreApply      = `Error running pre-apply plan: exit status 1`
-	ErrorNoEnvironmentFound   = `no environment found`
-	ErrorMultipleEnvironments = `multiple environments were returned`
-)
