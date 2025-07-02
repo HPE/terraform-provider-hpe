90369a94c9de2f92d3dc3c3717474475ecfd6b4f
Stuart McLaren <stuart.mclaren@hpe.com>
2025-07-02 12:13:22 +0100

    copyright

--- internal/subproviders/morpheus/convert/valuetoany.go
+++ internal/subproviders/morpheus/convert/valuetoany.go
@@ -1,6 +1,4 @@
 // (C) Copyright 2025 Hewlett Packard Enterprise Development LP
-// Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): 2025-07-01 17:11:11
-// Current User's Login: stuart-mclaren-hpe
 
 package convert
 
