8dce852a87429e35bafad936e7dd72603cee3938
naoise-osullivan-hpe <naoise.osullivan@hpe.com>
2025-07-23 11:17:13 +0100

    Update schema_gen.go

--- internal/subproviders/morpheus/resources/role/schema_gen.go
+++ internal/subproviders/morpheus/resources/role/schema_gen.go
@@ -4,7 +4,6 @@ package role
 
 import (
 	"context"
-
 	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
