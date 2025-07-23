bbcee3ce1ff6d70dfafc1ac677dd525d2c2f4492
naoise <naoise.osullivan@hpe.com>
2025-07-23 11:16:02 +0100

    fix diff in role schemas

--- internal/subproviders/morpheus/resources/role/schema_gen.go
+++ internal/subproviders/morpheus/resources/role/schema_gen.go
@@ -4,6 +4,7 @@ package role
 
 import (
 	"context"
+
 	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
