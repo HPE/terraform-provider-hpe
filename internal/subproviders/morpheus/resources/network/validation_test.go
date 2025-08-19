d4cac22118cbee82e68323e6f5c46ea3ba5e0249
Stuart McLaren <stuart.mclaren@hpe.com>
2025-08-19 17:35:07 +0100

    network resource: validation tests

---
+++ internal/subproviders/morpheus/resources/network/validation_test.go
@@ -0,0 +1,303 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+//go:build experimental
+
+package network_test
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/testhelpers"
+)
+
+// TestAccMorpheusNetworkResourceValidationMissingName tests that the
+// resource fails validation when the required 'name' field is missing
+func TestAccMorpheusNetworkResourceValidationMissingName(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration missing the required 'name' field
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	cloud_id = 1
+	group_id = 1
+	type_id  = 1
+	# name is missing - this should cause validation error
+}
+`
+
+	expected := `The argument "name" is required, but no definition was found`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationMissingCloudId tests that the
+// resource fails validation when the required 'cloud_id' field is missing
+func TestAccMorpheusNetworkResourceValidationMissingCloudId(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration missing the required 'cloud_id' field
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name     = "test-network"
+	group_id = 1
+	type_id  = 1
+	# cloud_id is missing - this should cause validation error
+}
+`
+
+	expected := `The argument "cloud_id" is required, but no definition was found`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationMissingGroupId tests that the
+// resource fails validation when the required 'group_id' field is missing
+func TestAccMorpheusNetworkResourceValidationMissingGroupId(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration missing the required 'group_id' field
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name     = "test-network"
+	cloud_id = 1
+	type_id  = 1
+	# group_id is missing - this should cause validation error
+}
+`
+
+	expected := `The argument "group_id" is required, but no definition was found`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationMissingTypeId tests that the
+// resource fails validation when the required 'type_id' field is missing
+func TestAccMorpheusNetworkResourceValidationMissingTypeId(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration missing the required 'type_id' field
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name     = "test-network"
+	cloud_id = 1
+	group_id = 1
+	# type_id is missing - this should cause validation error
+}
+`
+
+	expected := `The argument "type_id" is required, but no definition was found`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationInvalidConfig tests that the
+// resource fails validation when config field has invalid type (string
+// instead of object)
+func TestAccMorpheusNetworkResourceValidationInvalidConfig(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration with invalid config type
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name     = "test-network"
+	cloud_id = 1
+	group_id = 1
+	type_id  = 1
+	config   = "invalid"
+}
+`
+
+	expected := `attribute must be a valid object/map`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationInvalidPoolId tests that the
+// resource fails validation when pool_id field has invalid type (string
+// instead of int)
+func TestAccMorpheusNetworkResourceValidationInvalidPoolId(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration with invalid pool_id type
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name     = "test-network"
+	cloud_id = 1
+	group_id = 1
+	type_id  = 1
+	pool_id  = "not_valid_int"
+}
+`
+
+	expected := `Incorrect attribute value type`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationInvalidTenantIds tests that the
+// resource fails validation when tenant_ids field has invalid type
+// (string instead of set)
+func TestAccMorpheusNetworkResourceValidationInvalidTenantIds(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration with invalid tenant_ids type
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name       = "test-network"
+	cloud_id   = 1
+	group_id   = 1
+	type_id    = 1
+	tenant_ids = "not_valid_set"
+}
+`
+
+	expected := `set of number required`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationInvalidResourcePermissionsGroupIds
+// tests that the resource fails validation when resource_permissions.group_ids
+// contains non-integer values
+func TestAccMorpheusNetworkResourceValidationInvalidResourcePermissionsGroupIds(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration with invalid group_ids type in resource_permissions
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name     = "test-network"
+	cloud_id = 1
+	group_id = 1
+	type_id  = 1
+	resource_permissions = {
+		all = false
+		group_ids = ["a"]
+	}
+}
+`
+
+	expected := `Incorrect attribute value type`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      config,
+				PlanOnly:    true,
+				ExpectError: regexp.MustCompile(expected),
+			},
+		},
+	})
+}
+
+// TestAccMorpheusNetworkResourceValidationValidConfigNull tests that the
+// resource accepts null config value without validation error
+func TestAccMorpheusNetworkResourceValidationValidConfigNull(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	// Configuration with valid null config
+	config := providerConfig + `
+resource "hpe_morpheus_network" "test" {
+	name     = "test-network"
+	cloud_id = 1
+	group_id = 1
+	type_id  = 1
+	config   = null
+}
+`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:             config,
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
