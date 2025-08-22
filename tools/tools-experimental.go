17c3aa4b62f2b4bf9eaf669aaa824c2a1dc075e2
Stuart McLaren <stuart.mclaren@hpe.com>
2025-08-22 16:18:51 +0100

    docs: allow building in git worktree
    Allow running
    
    ```
    make docs
    make docs-experimental
    ```
    
    when the top level directory is not called terraform-provider-hpe
    (eg when using git worktrees)

--- tools/tools-experimental.go
+++ tools/tools-experimental.go
@@ -14,4 +14,4 @@ import (
 // to ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive ../examples/
 
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --website-source-dir templates-combined-temp --rendered-website-dir docs-experimental --examples-dir examples --provider-dir ..
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --website-source-dir templates-combined-temp --rendered-website-dir docs-experimental --examples-dir examples --provider-dir .. --provider-name terraform-provider-hpe
