0bd5eff2a69e49e1949abe8fda990f423a987676
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-09-03 12:39:22 +0100

    Keep service plan in experimental as well as release - fix CI

--- internal/subproviders/morpheus/datasources_experimental.go
+++ internal/subproviders/morpheus/datasources_experimental.go
@@ -26,6 +26,7 @@ import (
 	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/datasources/instancetypelayout"
 	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/datasources/network"
 	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/datasources/role"
+	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/datasources/serviceplan"
 )
 
 func (SubProvider) GetDataSources(
@@ -38,5 +39,6 @@ func (SubProvider) GetDataSources(
 		instancetypelayout.NewDataSource,
 		network.NewDataSource,
 		role.NewDataSource,
+		serviceplan.NewDataSource,
 	}
 }
