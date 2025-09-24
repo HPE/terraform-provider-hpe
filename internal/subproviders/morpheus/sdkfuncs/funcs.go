208c2c0976d97f45d69accd5bbce137fa1309a02
Jason Quigley <jason.quigley@hpe.com>
2025-09-24 12:24:19 +0100

    Add some sdk function aliases

---
+++ internal/subproviders/morpheus/sdkfuncs/funcs.go
@@ -0,0 +1,9 @@
+package sdkfuncs
+
+import "github.com/HewlettPackard/hpe-morpheus-go-sdk/sdk"
+
+var (
+	// Cloud
+	NewHvmCloudConfig        = sdk.NewAddCloudsRequestZoneConfigAnyOfOneOf2
+	AddCloudRequestHVMConfig = sdk.AddCloudsRequestZoneConfigAnyOfOneOf2AsAddCloudsRequestZoneConfigAnyOf
+)
