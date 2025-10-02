75d21b4b6d49870921ecb794eeca51bd5084590e
Jason Quigley <jason.quigley@hpe.com>
2025-10-02 14:04:54 +0100

    Clean up examples

--- internal/subproviders/morpheus/resources/cloud/example.tf
+++ internal/subproviders/morpheus/resources/cloud/example.tf
@@ -1,11 +1,8 @@
 resource "hpe_morpheus_cloud" "example" {
-  # Required fields
   name      = "TestCloud"
   tenant_id = 1
   group_id  = 1
 
-
-  # General configuration
   code             = "aCode"
   external_id      = "aCode"
   labels           = ["aLabel1", "aLabel2"]
@@ -14,23 +11,18 @@ resource "hpe_morpheus_cloud" "example" {
   location         = "somewhere"
   visibility       = "public"
 
-  # Agent and provisioning settings
   agent_install_mode       = "ssh"
   appliance_url            = "https://somewhere.com"
   auto_recover_power_state = true
   import_existing_vms      = "off"
 
-  # Cost and guidance settings
   costing_mode  = "costing"
   guidance_mode = "off"
 
-  # Security settings
   security_mode = "off"
 
-  # Console settings
   keyboard_layout = "us"
 
-  # HVM-specific configuration
   config_hvm = {
     certificate_provider          = "internal"
     enable_network_type_selection = false
