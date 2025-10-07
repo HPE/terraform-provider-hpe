1c508ccd9c440fae9a38167f644c3b119f068b39
Eamonn O'Toole <eamonn.otoole@hpe.com>
2025-10-07 10:35:07 +0100

    hpe_morpheus_datastore commit 2
    In this PR we add datastore_resource.go and datastore_import.go

---
+++ internal/subproviders/morpheus/resources/datastore/datastore_resource.go
@@ -0,0 +1,45 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+//go:build experimental
+
+package datastore
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/subproviders/morpheus/configure"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces.
+var (
+	_ resource.Resource                = &Resource{}
+	_ resource.ResourceWithImportState = &Resource{}
+)
+
+func NewResource() resource.Resource {
+	return &Resource{}
+}
+
+// Resource defines the resource implementation.
+type Resource struct {
+	configure.ResourceWithMorpheusConfigure
+	resource.Resource
+}
+
+func (r *Resource) Metadata(
+	_ context.Context,
+	req resource.MetadataRequest,
+	resp *resource.MetadataResponse,
+) {
+	resp.TypeName = req.ProviderTypeName + "_morpheus_datastore"
+}
+
+func (r *Resource) Schema(
+	ctx context.Context,
+	_ resource.SchemaRequest,
+	resp *resource.SchemaResponse,
+) {
+	resp.Schema = DatastoreResourceSchema(ctx)
+}
