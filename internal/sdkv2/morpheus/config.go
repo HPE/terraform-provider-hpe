e913b0cc561e5255d4f7df0a559f5e363b6aa39c
Eamonn O'Toole <eamonn.otoole@hpe.com>
2025-10-21 11:46:02 +0100

    Matthew's second change

--- internal/sdkv2/morpheus/config.go
+++ internal/sdkv2/morpheus/config.go
@@ -3,8 +3,6 @@
 package morpheus
 
 import (
-	"crypto/x509"
-	"errors"
 	"fmt"
 	"os"
 
@@ -23,41 +21,9 @@ type Config struct {
 	Password        string
 	ClientId        string
 	TenantSubdomain string
-	// Scope            string // "scope"
-	// GrantType            string  // "bearer"
-
-	Insecure bool
+	Insecure        bool
 
 	client *morpheus.Client
-}
-
-const sslCertErrorMsg = `
-You have enabled SSL certificate verification, but the certificate presented by
-the Morpheus server is not trusted. This could be due to a self-signed
-certificate or an internal certificate authority.
-
-We recommend fixing the certificate issue. If you need to bypass this check,
-proceed with caution and understand the security implications of doing so. You can
-disable certificate verification by either setting the provider argument
-"insecure = true" in your provider configuration.
-provider "hpe" {
-	morpheus {
-       url = "https://..."
-	   .
-	   .
-	   .
-	   insecure = true <-- set to true to disable certificate verification
-    }
-}
-`
-
-func certErrCallback(err error) error {
-	var certErr x509.UnknownAuthorityError
-	if errors.As(err, &certErr) {
-		return errors.New(certErr.Error() + sslCertErrorMsg)
-	}
-
-	return nil
 }
 
 func (c *Config) Client() (*morpheus.Client, diag.Diagnostics) {
@@ -68,25 +34,8 @@ func (c *Config) Client() (*morpheus.Client, diag.Diagnostics) {
 		var client *morpheus.Client
 		if c.Insecure {
 			client = morpheus.NewClient(c.Url, morpheus.WithDebug(debug), morpheus.Insecure())
-			diags = append(diags, diag.Diagnostic{
-				Severity: diag.Warning,
-				Summary:  "SSL Certificate Verification Disabled",
-				Detail: `
-SSL certificate verification is disabled. To enable verification, set "insecure = false" in your
-provider configuration.
-provider "hpe" {
-	morpheus { 
-       url = "https://..."
-	   .
-	   .
-	   .
-	   insecure = false <-- set to false to enable certificate verification
-    }
-}
-`,
-			})
 		} else {
-			client = morpheus.NewClient(c.Url, morpheus.WithDebug(debug), morpheus.WithErrCallbackFunc(certErrCallback))
+			client = morpheus.NewClient(c.Url, morpheus.WithDebug(debug))
 		}
 
 		// should validate url here too, and maybe ping it
