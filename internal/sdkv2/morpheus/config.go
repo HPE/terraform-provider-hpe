d4bb36e3d38352538669e57afc263a2cb45a0c80
Eamonn O'Toole <eamonn.otoole@hpe.com>
2025-10-21 11:56:05 +0100

    Matthew's change 3

--- internal/sdkv2/morpheus/config.go
+++ internal/sdkv2/morpheus/config.go
@@ -3,7 +3,6 @@
 package morpheus
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/gomorpheus/morpheus-go-sdk"
@@ -14,14 +13,15 @@ import (
 // Config is the configuration structure used to instantiate the Morpheus
 // provider.  Only Url and AccessToken are required.
 type Config struct {
-	Url             string
-	AccessToken     string
-	RefreshToken    string // optional and unused
-	Username        string
-	Password        string
-	ClientId        string
-	TenantSubdomain string
-	Insecure        bool
+	Url          string
+	AccessToken  string
+	RefreshToken string // optional and unused
+	Username     string
+	Password     string
+	ClientId     string
+	// TODO need to decide if we want to keep tenant subdomain here
+	// TenantSubdomain string
+	Insecure bool
 
 	client *morpheus.Client
 }
@@ -41,12 +41,13 @@ func (c *Config) Client() (*morpheus.Client, diag.Diagnostics) {
 		// should validate url here too, and maybe ping it
 		// logging with access token or username and password?
 		if c.Username != "" {
-			if c.TenantSubdomain != "" {
-				username := fmt.Sprintf(`%s\\%s`, c.TenantSubdomain, c.Username)
-				client.SetUsernameAndPassword(username, c.Password)
-			} else {
-				client.SetUsernameAndPassword(c.Username, c.Password)
-			}
+			// TODO need to decide if we want to keep tenant subdomain here
+			// if c.TenantSubdomain != "" {
+			// 	username := fmt.Sprintf(`%s\\%s`, c.TenantSubdomain, c.Username)
+			// 	client.SetUsernameAndPassword(username, c.Password)
+			// } else {
+			client.SetUsernameAndPassword(c.Username, c.Password)
+			// }
 		} else {
 			var expiresIn int64 = 86400 // lie (unused atm)
 			client.SetAccessToken(c.AccessToken, c.RefreshToken, expiresIn, "write")
