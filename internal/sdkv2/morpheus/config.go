671728cf8af92e722e2545389e8ee4dd032b7f37
Eamonn O'Toole <eamonn.otoole@hpe.com>
2025-10-21 11:59:53 +0100

    Switch to mux 2 (#373)

---
+++ internal/sdkv2/morpheus/config.go
@@ -0,0 +1,59 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package morpheus
+
+import (
+	"os"
+
+	"github.com/gomorpheus/morpheus-go-sdk"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/logging"
+)
+
+// Config is the configuration structure used to instantiate the Morpheus
+// provider.  Only Url and AccessToken are required.
+type Config struct {
+	Url          string
+	AccessToken  string
+	RefreshToken string // optional and unused
+	Username     string
+	Password     string
+	ClientId     string
+	// TODO need to decide if we want to keep tenant subdomain here
+	// TenantSubdomain string
+	Insecure bool
+
+	client *morpheus.Client
+}
+
+func (c *Config) Client() (*morpheus.Client, diag.Diagnostics) {
+	debug := logging.IsDebugOrHigher() && os.Getenv("MORPHEUS_API_HTTPTRACE") == "true"
+	diags := diag.Diagnostics{}
+
+	if c.client == nil {
+		var client *morpheus.Client
+		if c.Insecure {
+			client = morpheus.NewClient(c.Url, morpheus.WithDebug(debug), morpheus.Insecure())
+		} else {
+			client = morpheus.NewClient(c.Url, morpheus.WithDebug(debug))
+		}
+
+		// should validate url here too, and maybe ping it
+		// logging with access token or username and password?
+		if c.Username != "" {
+			// TODO need to decide if we want to keep tenant subdomain here
+			// if c.TenantSubdomain != "" {
+			// 	username := fmt.Sprintf(`%s\\%s`, c.TenantSubdomain, c.Username)
+			// 	client.SetUsernameAndPassword(username, c.Password)
+			// } else {
+			client.SetUsernameAndPassword(c.Username, c.Password)
+			// }
+		} else {
+			var expiresIn int64 = 86400 // lie (unused atm)
+			client.SetAccessToken(c.AccessToken, c.RefreshToken, expiresIn, "write")
+		}
+		c.client = client
+	}
+
+	return c.client, diags
+}
