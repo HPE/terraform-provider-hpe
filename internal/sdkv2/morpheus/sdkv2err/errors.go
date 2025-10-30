16fa5a2fdbca183e200a45b75adc5e43614a4ec7
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-10-30 16:59:37 +0000

    Add a builder function for reporting type assertion errors

---
+++ internal/sdkv2/morpheus/sdkv2err/errors.go
@@ -0,0 +1,13 @@
+package sdkv2err
+
+import (
+	"fmt"
+)
+
+// TypeAssertFail builds an error for reporting a failed type assertion.
+// It is useful for working with sdkv2 as it relies heavily on type
+// assertions to pull information from state and plan.
+// k is the key or name of the object being type asserted, v is its value.
+func TypeAssertFail(k string, v any) error {
+	return fmt.Errorf("%s: Type assertion failed for value: %v (type: %T)", k, v, v)
+}
