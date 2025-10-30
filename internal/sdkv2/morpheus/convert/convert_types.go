6f9984403a4c3ef6dde2106ea3989d78e8028ec7
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-10-30 17:55:49 +0000

    remove the comments

--- internal/sdkv2/morpheus/convert/convert_types.go
+++ internal/sdkv2/morpheus/convert/convert_types.go
@@ -21,38 +21,6 @@ func Int64ToString(n int64) string {
 	return strconv.FormatInt(n, 10)
 }
 
-// func ToString(i interface{}) string {
-// 	value, ok := i.(string)
-// 	if ok != true {
-// 		panic(fmt.Sprintf("ToString() failed to convert value '%v'", i)) // oh dear
-// 	}
-// 	return value
-// }
-
-// func ToBool(i interface{}) bool {
-// 	// if i == nil {
-// 	// 	return false
-// 	// }
-// 	value, ok := i.(bool)
-// 	if ok != true {
-// 		panic(fmt.Sprintf("ToString() failed to convert value '%v'", i)) // oh dear
-// 	}
-// 	return value
-// }
-
-// func StringToInt(v string) int {
-// 	value, _ := strconv.ParseInt(v, 10, 32)
-// 	return value
-// }
-
-// func StringToInt64(v string) int64 {
-// 	value, _ := strconv.ParseInt(v, 10, 64)
-// 	return value
-// }
-
-// This should all go away...please.
-// Maybe that stuff up there Too.
-
 // Bool returns a pointer To the bool value passed in.
 func Bool(v bool) *bool {
 	return &v
@@ -91,42 +59,3 @@ func BoolValueSlice(src []*bool) []bool {
 
 	return dst
 }
-
-// Int64 returns a pointer To the int64 value passed in.
-// func Int64(v int64) *int64 {
-// 	return &v
-// }
-
-// // Int64Value returns the value of the int64 pointer passed in or
-// // 0 if the pointer is nil.
-// func Int64Value(v *int64) int64 {
-// 	if v != nil {
-// 		return *v
-// 	}
-
-// 	return 0
-// }
-
-// // Int64Slice converts a slice of int64 values inTo a slice of
-// // int64 pointers
-// func Int64Slice(src []int64) []*int64 {
-// 	dst := make([]*int64, len(src))
-// 	for i := 0; i < len(src); i++ {
-// 		dst[i] = &(src[i])
-// 	}
-
-// 	return dst
-// }
-
-// // Int64ValueSlice converts a slice of int64 pointers inTo a slice of
-// // int64 values
-// func Int64ValueSlice(src []*int64) []int64 {
-// 	dst := make([]int64, len(src))
-// 	for i := 0; i < len(src); i++ {
-// 		if src[i] != nil {
-// 			dst[i] = *(src[i])
-// 		}
-// 	}
-
-// 	return dst
-// }
