3c9494aabc4eeee9ecd37008a5e52ed3e295844a
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-03 11:47:52 +0000

    update TypeAssertFail helper to include Error

--- internal/sdkv2/morpheus/helpers/errors.go
+++ internal/sdkv2/morpheus/helpers/errors.go
@@ -8,7 +8,7 @@ import (
 // It is useful for working with sdkv2 as it relies heavily on type
 // assertions to pull information from state and plan.
 // k is the key or name of the object being type asserted, v is its value.
-func TypeAssertFail(k string, v any) error {
+func TypeAssertFailError(k string, v any) error {
 	return fmt.Errorf("%s: Type assertion failed for value: %v (type: %T)", k, v, v)
 }
 
