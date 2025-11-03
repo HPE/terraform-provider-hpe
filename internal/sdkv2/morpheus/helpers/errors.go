1d934daf68f6b5b330c78afb3b6fc7c751266464
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-03 11:52:11 +0000

    Add error helpers for empty slices and nil pointers (#393)
    Adds two new error helpers for reporting when a slice is empty and for
    when a pointer is nil.
    
    These are two common occurrences in the legacy provider code that we
    need to defensively program against as we port resources.
    
    Drive-by: Rename `TypeAssertFail` to `TypeAssertFailError`

--- internal/sdkv2/morpheus/helpers/errors.go
+++ internal/sdkv2/morpheus/helpers/errors.go
@@ -8,6 +8,23 @@ import (
 // It is useful for working with sdkv2 as it relies heavily on type
 // assertions to pull information from state and plan.
 // k is the key or name of the object being type asserted, v is its value.
-func TypeAssertFail(k string, v any) error {
+func TypeAssertFailError(k string, v any) error {
 	return fmt.Errorf("%s: Type assertion failed for value: %v (type: %T)", k, v, v)
+}
+
+// EmptySliceAccess builds an error for reporting an attempt to access
+// an element from an empty slice.
+// It is useful for working with the legacy provider code as it often attempts
+// to access the 0th index of a slice without checking its length first.
+// k is the key or name of the slice being accessed.
+func EmptySliceError(k string) error {
+	return fmt.Errorf("%s: Slice is empty", k)
+}
+
+// NilPointer builds an error for reporting a nil pointer dereference.
+// It is useful for working with the legacy provider code as it often
+// dereferences pointers without checking for nil first.
+// k is the key or name of the pointer being dereferenced.
+func NilPointerError(k string) error {
+	return fmt.Errorf("%s: Pointer is nil", k)
 }
