f099d16dc1bb2398236db8e4555d5bbbb0e1d243
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-04 16:31:08 +0000

    Add sdkv2 resource: hpe_morpheus_task_javascript

---
+++ internal/sdkv2/morpheus/resources/task/resource_task_javascript.go
@@ -0,0 +1,458 @@
+package task
+
+import (
+	"context"
+	"log"
+
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/convert"
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/helpers"
+
+	morpheus "github.com/HewlettPackard/hpe-morpheus-go-sdk/legacy"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/structure"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func ResourceTaskJavaScript() *schema.Resource {
+	return &schema.Resource{
+		Description:   "Provides a Morpheus javascript task resource",
+		CreateContext: resourceTaskJavaScriptCreate,
+		ReadContext:   resourceTaskJavaScriptRead,
+		UpdateContext: resourceTaskJavaScriptUpdate,
+		DeleteContext: resourceTaskJavaScriptDelete,
+
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Description: "The ID of the javascript script task",
+				Computed:    true,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "The name of the javascript script task",
+				Required:    true,
+			},
+			"code": {
+				Type:        schema.TypeString,
+				Description: "The code of the javascript script task",
+				Optional:    true,
+			},
+			"labels": {
+				Type:        schema.TypeSet,
+				Description: "The organization labels associated with the task (Only supported on Morpheus 5.5.3 or higher)",
+				Optional:    true,
+				Computed:    true,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+			},
+			"result_type": {
+				Type:         schema.TypeString,
+				Description:  "The expected result type (single value, key pairs, json)",
+				ValidateFunc: validation.StringInSlice([]string{"value", "keyValue", "json"}, false),
+				Optional:     true,
+			},
+			"script_content": {
+				Type:        schema.TypeString,
+				Description: "The content of the javascript script",
+				Optional:    true,
+				StateFunc: func(v interface{}) string {
+					json, _ := structure.NormalizeJsonString(v)
+
+					return json
+				},
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					newJSON, _ := structure.NormalizeJsonString(new)
+					oldJSON, _ := structure.NormalizeJsonString(old)
+
+					return newJSON == oldJSON
+				},
+			},
+			"retryable": {
+				Type:        schema.TypeBool,
+				Description: "Whether to retry the task if there is a failure",
+				Optional:    true,
+				Default:     false,
+			},
+			"retry_count": {
+				Type:        schema.TypeInt,
+				Description: "The number of times to retry the task if there is a failure",
+				Optional:    true,
+				Default:     5,
+			},
+			"retry_delay_seconds": {
+				Type:        schema.TypeInt,
+				Description: "The number of seconds to wait between retry attempts",
+				Optional:    true,
+				Default:     10,
+			},
+			"allow_custom_config": {
+				Type:        schema.TypeBool,
+				Description: "Custom configuration data to pass during the execution of the javascript script",
+				Optional:    true,
+				Default:     false,
+			},
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+func resourceTaskJavaScriptCreate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	var client *morpheus.Client
+	if clientAssert, ok := meta.(*morpheus.Client); ok {
+		client = clientAssert
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("client", meta))
+	}
+
+	var name string
+	if nameValue, ok := d.Get("name").(string); ok {
+		name = nameValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("name", d.Get("name")))
+	}
+
+	taskOptions := make(map[string]any)
+
+	var scriptContent string
+	if scriptContentValue, ok := d.Get("script_content").(string); ok {
+		scriptContent = scriptContentValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("script_content", d.Get("script_content")))
+	}
+	taskOptions["jsScript"] = scriptContent
+
+	taskType := make(map[string]any)
+	taskType["code"] = "javascriptTask"
+
+	labelsPayload := make([]string, 0)
+	if attr, ok := d.GetOk("labels"); ok {
+		if labelSet, ok := attr.(*schema.Set); ok {
+			for _, s := range labelSet.List() {
+				if labelStr, ok := s.(string); ok {
+					labelsPayload = append(labelsPayload, labelStr)
+				} else {
+					return diag.FromErr(helpers.TypeAssertFailError("label", s))
+				}
+			}
+		} else {
+			return diag.FromErr(helpers.TypeAssertFailError("labels", attr))
+		}
+	}
+
+	var code string
+	if codeValue, ok := d.Get("code").(string); ok {
+		code = codeValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("code", d.Get("code")))
+	}
+
+	var resultType string
+	if resultTypeValue, ok := d.Get("result_type").(string); ok {
+		resultType = resultTypeValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("result_type", d.Get("result_type")))
+	}
+
+	var retryable bool
+	if retryableValue, ok := d.Get("retryable").(bool); ok {
+		retryable = retryableValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("retryable", d.Get("retryable")))
+	}
+
+	var allowCustomConfig bool
+	if allowCustomConfigValue, ok := d.Get("allow_custom_config").(bool); ok {
+		allowCustomConfig = allowCustomConfigValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("allow_custom_config", d.Get("allow_custom_config")))
+	}
+
+	var retryCount int
+	if retryCountValue, ok := d.Get("retry_count").(int); ok {
+		retryCount = retryCountValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("retry_count", d.Get("retry_count")))
+	}
+
+	var retryDelaySeconds int
+	if retryDelaySecondsValue, ok := d.Get("retry_delay_seconds").(int); ok {
+		retryDelaySeconds = retryDelaySecondsValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("retry_delay_seconds", d.Get("retry_delay_seconds")))
+	}
+
+	req := &morpheus.Request{
+		Body: map[string]any{
+			"task": map[string]any{
+				"name":              name,
+				"code":              code,
+				"labels":            labelsPayload,
+				"taskType":          taskType,
+				"taskOptions":       taskOptions,
+				"resultType":        resultType,
+				"executeTarget":     "local",
+				"retryable":         retryable,
+				"retryCount":        retryCount,
+				"retryDelaySeconds": retryDelaySeconds,
+				"allowCustomConfig": allowCustomConfig,
+			},
+		},
+	}
+	resp, err := client.CreateTask(req)
+	if err != nil {
+		log.Printf("API FAILURE: %s - %s", resp, err)
+
+		return diag.FromErr(err)
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	var result *morpheus.CreateTaskResult
+	if resultAssert, ok := resp.Result.(*morpheus.CreateTaskResult); ok {
+		result = resultAssert
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("resp.Result", resp.Result))
+	}
+
+	task := result.Task
+	// Successfully created resource, now set id
+	d.SetId(convert.Int64ToString(task.ID))
+	log.Printf("Task ID: %s", convert.Int64ToString(task.ID))
+
+	resourceTaskJavaScriptRead(ctx, d, meta)
+
+	return diags
+}
+
+func resourceTaskJavaScriptRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	var client *morpheus.Client
+	if clientAssert, ok := meta.(*morpheus.Client); ok {
+		client = clientAssert
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("client", meta))
+	}
+
+	id := d.Id()
+
+	var name string
+	if nameValue, ok := d.Get("name").(string); ok {
+		name = nameValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("name", d.Get("name")))
+	}
+
+	// lookup by name if we do not have an id yet
+	var resp *morpheus.Response
+	var err error
+	if id == "" && name != "" {
+		resp, err = client.FindTaskByName(name)
+	} else if id != "" {
+		resp, err = client.GetTask(convert.StringToInt64(id), &morpheus.Request{})
+	} else {
+		return diag.Errorf("Task cannot be read without name or id")
+	}
+
+	if err != nil {
+		// 404 is ok?
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("API 404: %s - %s", resp, err)
+			log.Printf("Forcing recreation of resource")
+			d.SetId("")
+
+			return diags
+		} else {
+			log.Printf("API FAILURE: %s - %s", resp, err)
+
+			return diag.FromErr(err)
+		}
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	// store resource data
+	var result *morpheus.GetTaskResult
+	if resultAssert, ok := resp.Result.(*morpheus.GetTaskResult); ok {
+		result = resultAssert
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("resp.Result", resp.Result))
+	}
+
+	javascriptTask := result.Task
+	d.SetId(convert.Int64ToString(javascriptTask.ID))
+	d.Set("name", javascriptTask.Name)
+	d.Set("code", javascriptTask.Code)
+	d.Set("labels", javascriptTask.Labels)
+	d.Set("script_content", javascriptTask.TaskOptions.JsScript)
+	d.Set("retryable", javascriptTask.Retryable)
+	d.Set("retry_count", javascriptTask.RetryCount)
+	d.Set("retry_delay_seconds", javascriptTask.RetryDelaySeconds)
+	d.Set("allow_custom_config", javascriptTask.AllowCustomConfig)
+
+	return diags
+}
+
+func resourceTaskJavaScriptUpdate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	var client *morpheus.Client
+	if clientAssert, ok := meta.(*morpheus.Client); ok {
+		client = clientAssert
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("client", meta))
+	}
+
+	id := d.Id()
+
+	var name string
+	if nameValue, ok := d.Get("name").(string); ok {
+		name = nameValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("name", d.Get("name")))
+	}
+
+	taskOptions := make(map[string]any)
+
+	var scriptContent string
+	if scriptContentValue, ok := d.Get("script_content").(string); ok {
+		scriptContent = scriptContentValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("script_content", d.Get("script_content")))
+	}
+	taskOptions["jsScript"] = scriptContent
+
+	taskType := make(map[string]any)
+	taskType["code"] = "javascriptTask"
+
+	labelsPayload := make([]string, 0)
+	if attr, ok := d.GetOk("labels"); ok {
+		if labelSet, ok := attr.(*schema.Set); ok {
+			for _, s := range labelSet.List() {
+				if labelStr, ok := s.(string); ok {
+					labelsPayload = append(labelsPayload, labelStr)
+				} else {
+					return diag.FromErr(helpers.TypeAssertFailError("label", s))
+				}
+			}
+		} else {
+			return diag.FromErr(helpers.TypeAssertFailError("labels", attr))
+		}
+	}
+
+	var code string
+	if codeValue, ok := d.Get("code").(string); ok {
+		code = codeValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("code", d.Get("code")))
+	}
+
+	var resultType string
+	if resultTypeValue, ok := d.Get("result_type").(string); ok {
+		resultType = resultTypeValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("result_type", d.Get("result_type")))
+	}
+
+	var retryable bool
+	if retryableValue, ok := d.Get("retryable").(bool); ok {
+		retryable = retryableValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("retryable", d.Get("retryable")))
+	}
+
+	var allowCustomConfig bool
+	if allowCustomConfigValue, ok := d.Get("allow_custom_config").(bool); ok {
+		allowCustomConfig = allowCustomConfigValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("allow_custom_config", d.Get("allow_custom_config")))
+	}
+
+	var retryCount int
+	if retryCountValue, ok := d.Get("retry_count").(int); ok {
+		retryCount = retryCountValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("retry_count", d.Get("retry_count")))
+	}
+
+	var retryDelaySeconds int
+	if retryDelaySecondsValue, ok := d.Get("retry_delay_seconds").(int); ok {
+		retryDelaySeconds = retryDelaySecondsValue
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("retry_delay_seconds", d.Get("retry_delay_seconds")))
+	}
+
+	req := &morpheus.Request{
+		Body: map[string]any{
+			"task": map[string]any{
+				"name":              name,
+				"code":              code,
+				"labels":            labelsPayload,
+				"taskType":          taskType,
+				"taskOptions":       taskOptions,
+				"resultType":        resultType,
+				"executeTarget":     "local",
+				"retryable":         retryable,
+				"retryCount":        retryCount,
+				"retryDelaySeconds": retryDelaySeconds,
+				"allowCustomConfig": allowCustomConfig,
+			},
+		},
+	}
+
+	resp, err := client.UpdateTask(convert.StringToInt64(id), req)
+	if err != nil {
+		log.Printf("API FAILURE: %s - %s", resp, err)
+
+		return diag.FromErr(err)
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	var result *morpheus.UpdateTaskResult
+	if resultAssert, ok := resp.Result.(*morpheus.UpdateTaskResult); ok {
+		result = resultAssert
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("resp.Result", resp.Result))
+	}
+
+	writeAttributesTask := result.Task
+	// Successfully updated resource, now set id
+	// err, it should not have changed though..
+	d.SetId(convert.Int64ToString(writeAttributesTask.ID))
+
+	return resourceTaskJavaScriptRead(ctx, d, meta)
+}
+
+func resourceTaskJavaScriptDelete(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	var client *morpheus.Client
+	if clientAssert, ok := meta.(*morpheus.Client); ok {
+		client = clientAssert
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("client", meta))
+	}
+
+	id := d.Id()
+	req := &morpheus.Request{}
+	resp, err := client.DeleteTask(convert.StringToInt64(id), req)
+	if err != nil {
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("API 404: %s - %s", resp, err)
+
+			return nil
+		} else {
+			log.Printf("API FAILURE: %s - %s", resp, err)
+
+			return diag.FromErr(err)
+		}
+	}
+	log.Printf("API RESPONSE: %s", resp)
+	d.SetId("")
+
+	return diags
+}
