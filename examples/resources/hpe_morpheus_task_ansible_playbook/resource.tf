c899106cdf9f946b2b938e5e2354c908878ad551
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-04 09:27:25 +0000

    Add sdkv2 resource: hpe_morpheus_task_ansible_playbook (#402)
    Adds the `hpe_morpheus_task_ansible_playbook` resource.
    
    The provider code has been updated to be type safe on type assertions.
    
    Some additional type assertions have been added for populating
    `taskOptions` (was previously unchecked `interface{}`s), as well as for
    populating the Morpheus `Request` struct.
    
    Adds template and generated docs.

---
+++ examples/resources/hpe_morpheus_task_ansible_playbook/resource.tf
@@ -0,0 +1,17 @@
+resource "hpe_morpheus_task_ansible_playbook" "ansplaybook" {
+  name                = "tfansibletest"
+  code                = "tfansibletest"
+  labels              = ["demo", "terraform"]
+  ansible_repo_id     = "5"
+  git_ref             = "master"
+  playbook            = "mongo_install"
+  tags                = "mongo"
+  skip_tags           = "web"
+  command_options     = "-b"
+  execute_target      = "local"
+  retryable           = true
+  retry_count         = 1
+  retry_delay_seconds = 10
+  allow_custom_config = true
+}
+
