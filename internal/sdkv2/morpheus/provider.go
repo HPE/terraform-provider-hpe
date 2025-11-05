06f66a568533def084ffd883eb8b32e27b209467
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-05 16:12:44 +0000

    Add sdkv2 resource: hpe_morpheus_task_javascript (#410)
    Adds the `hpe_morpheus_task_javascript` resource.
    
    The provider code has been updated to be type safe on type assertions.
    
    Some additional type assertions have been added for populating
    taskOptions (was previously unchecked interface{}s), as well as for
    populating the Morpheus Request struct.
    
    Adds template and generated docs.

--- internal/sdkv2/morpheus/provider.go
+++ internal/sdkv2/morpheus/provider.go
@@ -26,6 +26,7 @@ func Provider() *schema.Provider {
 			"hpe_morpheus_task_chef_bootstrap":              task.ResourceTaskChefBootstrap(),
 			"hpe_morpheus_task_email":                       task.ResourceTaskEmail(),
 			"hpe_morpheus_task_groovy_script":               task.ResourceTaskGroovyScript(),
+			"hpe_morpheus_task_javascript":                  task.ResourceTaskJavaScript(),
 		},
 		DataSourcesMap:       map[string]*schema.Resource{},
 		ConfigureContextFunc: providerConfigure,
