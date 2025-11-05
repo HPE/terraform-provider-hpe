2e9144edf4d43c21b064c6396afe92125c837a95
ZoeySimone <78411653+ZoeySimone@users.noreply.github.com>
2025-11-05 15:30:45 +0000

    Move image resource out of experimental (#429)

--- internal/framework/subproviders/morpheus/resources/image/create.go
+++ internal/framework/subproviders/morpheus/resources/image/create.go
@@ -1,7 +1,5 @@
 // (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 
-//go:build experimental
-
 package image
 
 import (
@@ -252,7 +250,10 @@ func (r *Resource) Create(ctx context.Context, req resource.CreateRequest, resp 
 	// 	return
 	// }
 
-	// _, httpResp, err := client.LibraryAPI.AddVirtualImageFile(ctx, plan.Id.ValueInt64()).Filename(plan.File.ValueString()).Body(file).Execute()
+	// _, httpResp, err := client.LibraryAPI.AddVirtualImageFile(
+	// 	ctx,
+	// 	plan.Id.ValueInt64(),
+	// ).Filename(plan.File.ValueString()).Body(file).Execute()
 	// if err != nil || httpResp.StatusCode != http.StatusOK {
 	// 	resp.Diagnostics.AddError("error uploading image file", errors.ErrMsg(err, httpResp))
 
