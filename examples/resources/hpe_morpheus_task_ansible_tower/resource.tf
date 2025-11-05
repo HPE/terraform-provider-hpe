64cc9a6ee8df17151e44f47c953512ab2a00ad1d
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-05 15:19:41 +0000

    Add sdkv2 resource: hpe_morpheus_task_ansible_tower (#403)
    Adds the hpe_morpheus_task_ansible_playbook resource.
    
    The provider code has been updated to be type safe on type assertions.
    
    Some additional type assertions have been added for populating
    taskOptions (was previously unchecked interface{}s), as well as for
    populating the Morpheus Request struct.
    
    Adds template and generated docs.
    
    Removed usage of `result_type` from the code as it's not in the schema.
    Resource description updated (was incorrectly using the vRO task
    description).
    
    ---------
    
    Co-authored-by: Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>

---
+++ examples/resources/hpe_morpheus_task_ansible_tower/resource.tf
@@ -0,0 +1,17 @@
+resource "hpe_morpheus_task_ansible_tower" "tfexample_task_ansible_tower" {
+  name                         = "tfexample_task_ansible_tower"
+  code                         = "tfexample-ansible-tower-task"
+  labels                       = ["demo", "terraform"]
+  ansible_tower_integration_id = 1
+  ansible_tower_inventory_id   = 5
+  group                        = "demo"
+  job_template_id              = 3
+  scm_override                 = "main"
+  execute_mode                 = "executeAll"
+  execute_target               = "local"
+  retryable                    = true
+  retry_count                  = 5
+  retry_delay_seconds          = 10
+  allow_custom_config          = true
+  visibility                   = "public"
+}
