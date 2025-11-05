b7a6e9170aef4642acc3f5046b4e1572dbedd052
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-05 16:48:52 +0000

    Add sdkv2 resource: hpe_morpheus_task_powershell_script (#416)
    Adds the `hpe_morpheus_task_powershell_script` resource.
    
    The provider code has been updated to be type safe on type assertions.
    
    Some additional type assertions have been added for populating
    taskOptions (was previously unchecked interface{}s), as well as for
    populating the Morpheus Request struct.
    
    Adds template and generated docs.

--- internal/sdkv2/morpheus/provider.go
+++ internal/sdkv2/morpheus/provider.go
@@ -30,6 +30,7 @@ func Provider() *schema.Provider {
 			"hpe_morpheus_task_library_script":              task.ResourceTaskLibraryScript(),
 			"hpe_morpheus_task_library_template":            task.ResourceTaskLibraryTemplate(),
 			"hpe_morpheus_task_nested_workflow":             task.ResourceTaskNestedWorkflow(),
+			"hpe_morpheus_task_powershell_script":           task.ResourceTaskPowerShellScript(),
 		},
 		DataSourcesMap:       map[string]*schema.Resource{},
 		ConfigureContextFunc: providerConfigure,
