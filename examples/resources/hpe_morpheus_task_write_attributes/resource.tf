75791605fe19898839298e8c535372daef592c32
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-05 17:29:09 +0000

    Add sdkv2 resource: hpe_morpheus_task_write_attributes (#426)
    Adds the `hpe_morpheus_task_write_attributes` resource.
    
    The provider code has been updated to be type safe on type assertions.
    
    Some additional type assertions have been added for populating
    taskOptions (was previously unchecked interface{}s), as well as for
    populating the Morpheus Request struct.
    
    Adds template and generated docs.

---
+++ examples/resources/hpe_morpheus_task_write_attributes/resource.tf
@@ -0,0 +1,12 @@
+resource "hpe_morpheus_task_write_attributes" "tfexample_write_attributes" {
+  name                = "tfexample_write_attributes"
+  code                = "tfexample_write_attributes"
+  labels              = ["demo", "terraform"]
+  attributes          = <<EOF
+{"demo":"test"}
+EOF
+  retryable           = true
+  retry_count         = 1
+  retry_delay_seconds = 10
+  allow_custom_config = true
+}
