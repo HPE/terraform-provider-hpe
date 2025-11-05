ae8df20e1ff23f815e7e787cd9e8a9fe23d385e7
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-05 17:06:05 +0000

    Add sdkv2 resource: hpe_morpheus_task_ruby_script (#422)
    Adds the `hpe_morpheus_task_ruby_script` resource.
    
    The provider code has been updated to be type safe on type assertions.
    
    Some additional type assertions have been added for populating
    taskOptions (was previously unchecked interface{}s), as well as for
    populating the Morpheus Request struct.
    
    Adds template and generated docs.

---
+++ examples/resources/hpe_morpheus_task_ruby_script/resource.tf
@@ -0,0 +1,13 @@
+resource "hpe_morpheus_task_ruby_script" "tfexample_ruby_local" {
+  name                = "tfexample_ruby_local"
+  code                = "tfexample_ruby_local"
+  labels              = ["demo", "terraform"]
+  source_type         = "local"
+  script_content      = <<EOF
+  puts "testing"
+EOF
+  retryable           = true
+  retry_count         = 1
+  retry_delay_seconds = 10
+  allow_custom_config = true
+}
