0e730a4c2eb8b6a6dfc683c37abdbe70bc5b9888
Ryan Davis <ryan.davis@hpe.com>
2025-11-06 17:18:36 +0000

    Short mode fix

--- internal/framework/subproviders/morpheus/test/sweep/sweep_test.go
+++ internal/framework/subproviders/morpheus/test/sweep/sweep_test.go
@@ -13,6 +13,12 @@ import (
 )
 
 func init() {
+	// Skip sweep client creation and sweeper registration in short mode
+	if testing.Short() {
+		log.Printf("[INFO] Skipping sweeper registration in short mode")
+		return
+	}
+
 	ctx := context.Background()
 	client, err := NewSweepClient(ctx)
 	if err != nil {
