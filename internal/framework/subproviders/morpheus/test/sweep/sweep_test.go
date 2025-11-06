5fac3d4158f899c41af8e5c8f594f3b3e9e3780d
Ryan Davis <ryan.davis@hpe.com>
2025-11-06 16:59:25 +0000

    Further refactoring for better client handling

--- internal/framework/subproviders/morpheus/test/sweep/sweep_test.go
+++ internal/framework/subproviders/morpheus/test/sweep/sweep_test.go
@@ -3,16 +3,29 @@
 package sweep
 
 import (
+	"context"
+	"log"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/resources/policy"
 )
 
 func init() {
+	ctx := context.Background()
+	client, err := NewSweepClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create sweep client: %v", err)
+	}
+
+	// Register sweepers that use the centralized client
+	policy.NewPolicySweeper(client)
+
+	// Register sweepers that still create their own clients (to be converted later)
 	Datastores()
 	Instances()
 	Networks()
-	Policies()
 	Users()
 }
 
