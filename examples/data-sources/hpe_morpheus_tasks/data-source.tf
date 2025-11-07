90ebaee667a3ff0ed850f2f7519c363473385677
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-07 13:41:07 +0000

    Add sdkv2 data source: Tasks (#444)
    Adds the `hpe_morpheus_tasks` data source.
    
    The provider has been updated to be type safe on type assertions.
    
    Adds template and generated docs.

---
+++ examples/data-sources/hpe_morpheus_tasks/data-source.tf
@@ -0,0 +1,14 @@
+data "hpe_morpheus_tasks" "example_tasks" {
+  sort_ascending = true
+
+  filter {
+    name   = "name"
+    values = ["Test*"]
+  }
+
+  filter {
+    name   = "type"
+    values = ["Shell Script", "Python Script"]
+  }
+}
+
