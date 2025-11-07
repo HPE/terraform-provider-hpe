48494d1da5181d1230009f252ac122ceb5e64119
Jason Quigley <jason.quigley@hpe.com>
2025-11-07 19:26:43 +0000

    sdkv2 integration_ansible_tower

--- internal/sdkv2/morpheus/provider.go
+++ internal/sdkv2/morpheus/provider.go
@@ -27,6 +27,7 @@ func Provider() *schema.Provider {
 			"hpe_morpheus_identity_source_active_directory": identitysource.ResourceIdentitySourceActiveDirectory(),
 			"hpe_morpheus_identity_source_saml":             identitysource.ResourceIdentitySourceSAML(),
 			"hpe_morpheus_integration_ansible":              integration.ResourceIntegrationAnsible(),
+			"hpe_morpheus_integration_ansible_tower":        integration.ResourceIntegrationAnsibleTower(),
 			"hpe_morpheus_task_ansible_playbook":            task.ResourceTaskAnsiblePlaybook(),
 			"hpe_morpheus_task_ansible_tower":               task.ResourceTaskAnsibleTower(),
 			"hpe_morpheus_task_chef_bootstrap":              task.ResourceTaskChefBootstrap(),
