782adb102781dd8b47be7d90555df175cba49846
Jason Quigley <jason.quigley@hpe.com>
2025-11-07 19:15:40 +0000

    sdkv2 add integration_ansible

--- internal/sdkv2/morpheus/provider.go
+++ internal/sdkv2/morpheus/provider.go
@@ -12,6 +12,7 @@ import (
 	tasksdatasource "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/datasources/tasks"
 	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/cluster"
 	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/identitysource"
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/integration"
 	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/task"
 	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/wiki"
 	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/workflow"
@@ -25,6 +26,7 @@ func Provider() *schema.Provider {
 			"hpe_morpheus_cluster_mks_vsphere":              cluster.ResourceClusterMKSVSphere(),
 			"hpe_morpheus_identity_source_active_directory": identitysource.ResourceIdentitySourceActiveDirectory(),
 			"hpe_morpheus_identity_source_saml":             identitysource.ResourceIdentitySourceSAML(),
+			"hpe_morpheus_integration_ansible":              integration.ResourceIntegrationAnsible(),
 			"hpe_morpheus_task_ansible_playbook":            task.ResourceTaskAnsiblePlaybook(),
 			"hpe_morpheus_task_ansible_tower":               task.ResourceTaskAnsibleTower(),
 			"hpe_morpheus_task_chef_bootstrap":              task.ResourceTaskChefBootstrap(),
