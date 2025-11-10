2b500df0d930be5302b9a6183196b8c1e410a376
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-10 12:04:50 +0000

    Add sdkv2 resource: hpe_morpheus_job_workflow (#451)
    Adds the `hpe_morpheus_job_workflow` data source.
    
    The provider has been updated to be type safe on type assertions.
    
    Adds template and generated docs.

---
+++ examples/resources/morpheus_job_workflow/resource_schedule.tf
@@ -0,0 +1,13 @@
+resource "hpe_morpheus_job_workflow" "tf_example_workflow_job_date_and_time" {
+  name                  = "TF Example Workflow Job Schedule"
+  enabled               = true
+  labels                = ["aws", "demo"]
+  workflow_id           = 1
+  schedule_mode         = "scheduled"
+  execution_schedule_id = 1
+  context_type          = "instance"
+  instance_ids          = [91]
+  custom_options = {
+    "demo" = "testing"
+  }
+}
