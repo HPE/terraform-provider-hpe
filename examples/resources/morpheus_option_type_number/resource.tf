7318b4df7115dbb5b573626dc49324ef444c243b
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2025-11-12 18:35:58 +0000

    sdkv2 option_type_number (#492)

---
+++ examples/resources/morpheus_option_type_number/resource.tf
@@ -0,0 +1,18 @@
+resource "hpe_morpheus_option_type_number" "tf_example_number_option_type" {
+  name                     = "tf_example_number_option_type"
+  description              = "Terraform number option type example"
+  labels                   = ["demo", "terraform"]
+  field_name               = "tfNumberExample"
+  export_meta              = true
+  dependent_field          = "dependent_example"
+  visibility_field         = "visibility_example"
+  require_field            = "require_example"
+  show_on_edit             = true
+  editable                 = true
+  display_value_on_details = true
+  field_label              = "Number Example"
+  placeholder              = "12"
+  default_value            = "1"
+  help_block               = "Provide a number"
+  required                 = true
+}
