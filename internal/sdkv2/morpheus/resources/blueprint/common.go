57dc33b9025dd67c9cb6eaf349ef102b09a9fc9e
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-17 11:34:43 +0000

    Add sdkv2 resource: hpe_morpheus_instance_type_layout (#547)
    Adds the `hpe_morpheus_instance_type_layout` resource.
    
    The provider has been updated to be type safe on type assertions.
    
    Adds template docs.
    
    Registration of this and related resources in provider.go will follow in
    a separate PR, alongside generated documentation.
    
    Drive-by: add copyright header to blueprints common.go

--- internal/sdkv2/morpheus/resources/blueprint/common.go
+++ internal/sdkv2/morpheus/resources/blueprint/common.go
@@ -1,10 +1,12 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
 package blueprint
 
 // This cannot currently be handled efficiently by a DiffSuppressFunc.
 // See: https://github.com/hashicorp/terraform-plugin-sdk/issues/477
 //
 //nolint:unused
-func matchTemplatesWithSchema(templates []int64, declaredTemplates []interface{}) []int64 {
+func matchTemplatesWithSchema(templates []int64, declaredTemplates []any) []int64 {
 	result := make([]int64, len(declaredTemplates))
 
 	rMap := make(map[int64]int64, len(templates))
@@ -13,12 +15,13 @@ func matchTemplatesWithSchema(templates []int64, declaredTemplates []interface{}
 	}
 
 	for i, definedTemplate := range declaredTemplates {
-		definedTemplate := int64(definedTemplate.(int))
-
-		if v, ok := rMap[definedTemplate]; ok {
-			// matched node type declared by ID
-			result[i] = v
-			delete(rMap, v)
+		// skip if type assertion failed
+		if definedTemplateInt64, ok := definedTemplate.(int64); ok {
+			if v, ok := rMap[definedTemplateInt64]; ok {
+				// matched node type declared by ID
+				result[i] = v
+				delete(rMap, v)
+			}
 		}
 	}
 	// append unmatched node type to the result
