5929e85eda3987e4eab4ec5a982bda031e822d69
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-11-18 16:15:39 +0000

    Add sdkv2 data source: hpe_morpheus_integration

---
+++ internal/sdkv2/morpheus/datasources/integration/integration.go
@@ -0,0 +1,106 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package integration
+
+import (
+	"context"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	morpheus "github.com/HewlettPackard/hpe-morpheus-go-sdk/legacy"
+
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/convert"
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/helpers"
+)
+
+func DataSourceIntegration() *schema.Resource {
+	return &schema.Resource{
+		Description: "Provides a Morpheus integration data source.",
+		ReadContext: dataSourceIntegrationRead,
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:          schema.TypeInt,
+				Optional:      true,
+				ConflictsWith: []string{"name"},
+				Computed:      true,
+			},
+			"name": {
+				Type:          schema.TypeString,
+				Description:   "The name of the integration",
+				Optional:      true,
+				ConflictsWith: []string{"id"},
+			},
+		},
+	}
+}
+
+func dataSourceIntegrationRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	var client *morpheus.Client
+	if v, ok := meta.(*morpheus.Client); ok {
+		client = v
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("client", meta))
+	}
+
+	var diags diag.Diagnostics
+
+	var name string
+	if v, ok := d.Get("name").(string); ok {
+		name = v
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("name", d.Get("name")))
+	}
+
+	var id int
+	if v, ok := d.Get("id").(int); ok {
+		id = v
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("id", d.Get("id")))
+	}
+
+	var resp *morpheus.Response
+	var err error
+	if id == 0 && name != "" {
+		resp, err = client.FindIntegrationByName(name)
+	} else if id != 0 {
+		resp, err = client.GetIntegration(int64(id), &morpheus.Request{})
+	} else {
+		return diag.Errorf("Integration cannot be read without name or id")
+	}
+
+	if err != nil {
+		if resp != nil && resp.StatusCode == 404 {
+			log.Printf("API 404: %s - %v", resp, err)
+
+			return nil
+		}
+
+		log.Printf("API FAILURE: %s - %v", resp, err)
+
+		return diag.FromErr(err)
+	}
+	log.Printf("API RESPONSE: %s", resp)
+
+	if resp.Result == nil {
+		return diag.FromErr(helpers.NotFoundInResponseError("Result"))
+	}
+
+	var result *morpheus.GetIntegrationResult
+	if v, ok := resp.Result.(*morpheus.GetIntegrationResult); ok {
+		result = v
+	} else {
+		return diag.FromErr(helpers.TypeAssertFailError("Result", resp.Result))
+	}
+
+	if result.Integration == nil {
+		return diag.FromErr(helpers.NotFoundInResponseError("Integration"))
+	}
+
+	integration := result.Integration
+	d.SetId(convert.Int64ToString(integration.ID))
+	d.Set("name", integration.Name)
+
+	return diags
+}
