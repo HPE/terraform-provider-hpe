625480b0f931d81239bfd48bc65932f371893dea
Eamonn O'Toole <eamonn.otoole@hpe.com>
2025-11-27 14:14:46 +0000

    Add another instance example
    We've added another instance example, this time one for BMaaS.

---
+++ internal/framework/subproviders/morpheus/resources/instance/example_metal.tf
@@ -0,0 +1,95 @@
+data "hpe_morpheus_cloud" "test" {
+  name = "aCloud"
+}
+
+data "hpe_morpheus_environment" "test" {
+  name = "anEnvironment"
+}
+
+data "hpe_morpheus_group" "test" {
+  name = "aGroup"
+}
+
+data "hpe_morpheus_instance_type_layout" "test" {
+  name = "Single ILO Server"
+}
+
+data "hpe_morpheus_role" "test" {
+  name = "aRole"
+}
+
+data "hpe_morpheus_service_plan" "tp" {
+    name                = "G3i"
+    provision_type_code = "hpe-baremetal-plugin.provision"
+}
+
+resource "hpe_morpheus_instance" "example" {
+  name             = "TestInstance"
+  cloud_id         = data.hpe_morpheus_cloud.test.id # BM Cloud
+  layout_id        = data.hpe_morpheus_instance_type_layout.test.id
+  instance_type_id = 58 # BM Instance
+
+  group_id = data.hpe_morpheus_group.test.id
+  plan_id  = data.hpe_morpheus_service_plan.tp.id
+
+  instance_context = "dev"
+
+  network_interfaces = [
+    {
+      network_id      = 4
+      network_type_id = 18
+      ip_pool         = 1
+      child_virtual_networks = [
+        {
+          network_type_id = 19
+          network_id      = 4
+          ip_pool         = 1
+        }
+      ]
+    },
+    {
+      network_id      = 4
+      ip_pool         = 1
+      network_type_id = 18
+    },
+  ]
+
+  volumes = [
+    {
+      root_volume      = true
+      name             = "root"
+      size             = 10
+      storage_type_id  = 1
+      datastore_id     = 2
+    }
+  ]
+
+  tags = [
+    {
+      name  = "terraform"
+      value = "true"
+    },
+    {
+      name  = "acctest"
+      value = "true"
+    },
+    {
+      name  = "hpe_morpheus_instance"
+      value = "true"
+    },
+    {
+      name  = "mytag"
+      value = "true"
+    },
+    {
+      name  = "jesskey"
+      value = "terraform"
+    }
+  ]
+
+  config = {
+    noAgent    = true
+    createUser = false
+    imageId    = 189
+  }
+}
