242b0feb893656367143db6ec69cdef0b952ac0c
Ryan Davis <ryan.davis@hpe.com>
2025-11-28 12:53:27 +0000

    Adding acceptance tests for validation in resource.go

--- internal/framework/subproviders/morpheus/resources/policy/resource_test.go
+++ internal/framework/subproviders/morpheus/resources/policy/resource_test.go
@@ -161,3 +161,125 @@ resource "hpe_morpheus_policy" "validation_test" {
 		},
 	})
 }
+
+// Test validation: incompatible policy type and resource type (motd does not support User)
+func TestAccMorpheusPolicyValidationIncompatiblePolicyAndResourceType(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	t.Parallel()
+
+	providerConfig := testhelpers.ProviderBlockMixed()
+	name := acctest.RandomWithPrefix(t.Name())
+	roleName := acctest.RandomWithPrefix(t.Name() + "-role")
+	userName := acctest.RandomWithPrefix(t.Name() + "-user")
+
+	resourceConfig := `
+resource "hpe_morpheus_role" "test" {
+  name = "` + roleName + `"
+  role_type = "user"
+}
+
+resource "hpe_morpheus_user" "test" {
+  username = "` + userName + `"
+  email = "` + userName + `@test.com"
+  role_ids = [hpe_morpheus_role.test.id]
+  password_wo = "TestPassword123!"
+}
+
+resource "hpe_morpheus_policy" "validation_test" {
+  name = "` + name + `"
+  associated_resource_type = "User"
+  associated_resource_id = hpe_morpheus_user.test.id
+  
+  policy_type = {
+    code = "motd"
+  }
+  
+  config = {
+    enabled = true
+    message = "Test message"
+    fullPage = false
+    title = "MOTD"
+  }
+}
+`
+
+	resource.Test(t, resource.TestCase{
+		ExternalProviders: map[string]resource.ExternalProvider{
+			"morpheus": {
+				Source:            "gomorpheus/morpheus",
+				VersionConstraint: "0.13.2",
+			},
+		},
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      providerConfig + resourceConfig,
+				ExpectError: regexp.MustCompile("Incompatible policy type and resource type"),
+			},
+		},
+	})
+}
+
+// Test validation: tenants not supported for policy type
+func TestAccMorpheusPolicyValidationTenantsNotSupported(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	t.Parallel()
+
+	providerConfig := testhelpers.ProviderBlock()
+	name := acctest.RandomWithPrefix(t.Name())
+	groupName := acctest.RandomWithPrefix(t.Name() + "-group")
+	cloudName := acctest.RandomWithPrefix(t.Name() + "-cloud")
+
+	resourceConfig := `
+resource "hpe_morpheus_group" "test" {
+  name = "` + groupName + `"
+  location = "test"
+}
+
+resource "hpe_morpheus_cloud" "test" {
+  name = "` + cloudName + `"
+  tenant_id = 1
+  group_id = hpe_morpheus_group.test.id
+  code = "` + cloudName + `"
+  cloud_type_code = "standard"
+  
+  config = {
+    certificateProvider = "internal"
+    enableNetworkTypeSelection = false
+  }
+}
+
+resource "hpe_morpheus_policy" "validation_test" {
+  name = "` + name + `"
+  associated_resource_type = "Cloud"
+  associated_resource_id = hpe_morpheus_cloud.test.id
+  tenants = [1]
+  
+  policy_type = {
+    code = "requiredNetwork"
+  }
+  
+  config = {
+    requiredNetworks = [1]
+  }
+}
+`
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      providerConfig + resourceConfig,
+				ExpectError: regexp.MustCompile("Tenants not supported for this policy type"),
+			},
+		},
+	})
+}
