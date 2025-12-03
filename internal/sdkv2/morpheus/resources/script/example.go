1f7b603e2d86bfecf83048b7f91d6a57d46fd79c
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-03 16:38:46 +0000

    Parity: morpheus_boot_script tests

---
+++ internal/sdkv2/morpheus/resources/script/example.go
@@ -0,0 +1,5 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package script
+
+//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_boot_script/resource.tf boot_script_resource.tf.tmpl Name "TF Example Boot Script" Content "ls"
