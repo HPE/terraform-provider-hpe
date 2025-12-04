efe1dc7a588d8e29ff52dfa225f06c669a1f62a7
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-04 15:16:32 +0000

    fix the resource name in test

--- internal/sdkv2/morpheus/resources/template/cluster_package_resource_test.go
+++ internal/sdkv2/morpheus/resources/template/cluster_package_resource_test.go
@@ -64,57 +64,57 @@ func TestAccMorpheusClusterPackageExampleOk(t *testing.T) {
 
 	checks := []resource.TestCheckFunc{
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"name",
 			name,
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"code",
 			"tf-example-cluster-package",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"description",
 			"Terraform example cluster package",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"package_version",
 			"1.2.3",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"type",
 			"apps",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"package_type",
 			"example",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"enabled",
 			"true",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"repeat_install",
 			"true",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"spec_template_ids.#",
 			"2",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"spec_template_ids.0",
 			"1",
 		),
 		resource.TestCheckResourceAttr(
-			"hpe_morpheus_cluster_package."+name,
+			"hpe_morpheus_cluster_package.tf_example_cluster_package",
 			"spec_template_ids.1",
 			"2",
 		),
