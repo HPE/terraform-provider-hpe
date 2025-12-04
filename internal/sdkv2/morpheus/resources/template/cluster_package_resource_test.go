b757f9a83c3e0165fee409eb9d0a478493ab2069
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-04 15:09:38 +0000

    Parity: cluster_package tests

---
+++ internal/sdkv2/morpheus/resources/template/cluster_package_resource_test.go
@@ -0,0 +1,147 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func newProviderWithError() (tfprotov6.ProviderServer, error) {
+	return tf5to6server.UpgradeServer(context.Background(), sdkv2morpheus.Provider().GRPCProvider)
+}
+
+var testAccProtoV6ProviderFactories = map[string]func() (
+	tfprotov6.ProviderServer, error,
+){
+	"hpe": newProviderWithError,
+}
+
+func TestAccMorpheusClusterPackageExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := testhelpers.RenderExample(t, "cluster_package_resource.tf.tmpl",
+		"Name", name,
+		"Code", "tf-example-cluster-package",
+		"Description", "Terraform example cluster package",
+		"PackageVersion", "1.2.3",
+		"Type", "apps",
+		"PackageType", "example",
+		"Enabled", "true",
+		"RepeatInstall", "true",
+		"SpecTemplateIds", "[1,2]",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"code",
+			"tf-example-cluster-package",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"description",
+			"Terraform example cluster package",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"package_version",
+			"1.2.3",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"type",
+			"apps",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"package_type",
+			"example",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"enabled",
+			"true",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"repeat_install",
+			"true",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"spec_template_ids.#",
+			"2",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"spec_template_ids.0",
+			"1",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_cluster_package."+name,
+			"spec_template_ids.1",
+			"2",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Plan
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			// Apply
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			// Plan after apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
