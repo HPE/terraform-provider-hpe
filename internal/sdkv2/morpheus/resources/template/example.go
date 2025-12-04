fed32080bc1ddb3a995c378ae6da4e044e2d24a8
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-04 17:39:24 +0000

    fix stuttering resource names

--- internal/sdkv2/morpheus/resources/template/example.go
+++ internal/sdkv2/morpheus/resources/template/example.go
@@ -2,4 +2,4 @@
 
 package template
 
-//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_hpe_morpheus_script_template/resource.tf hpe_morpheus_script_template_resource.tf.tmpl Name tf-terraform-script-template Labels "[\"demo\", \"template\", \"terraform\"]" ScriptType bash ScriptPhase provision ScriptContent "echo \"testing\"" RunAsUser root Sudo true
+//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_script_template/resource.tf hpe_morpheus_script_template_resource.tf.tmpl Name tf-terraform-script-template Labels "[\"demo\", \"template\", \"terraform\"]" ScriptType bash ScriptPhase provision ScriptContent "echo \"testing\"" RunAsUser root Sudo true
