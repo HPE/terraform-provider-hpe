e8f820c3c8a3a72c4a9e164c20c3ff1ad4cac308
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-04 17:17:53 +0000

    Parity: hpe_morpheus_script_template tests

---
+++ internal/sdkv2/morpheus/resources/template/hpe_morpheus_script_template_resource_test.go
@@ -0,0 +1,140 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func newProviderWithError() (tfprotov6.ProviderServer, error) {
+	return tf5to6server.UpgradeServer(context.Background(), sdkv2morpheus.Provider().GRPCProvider)
+}
+
+var testAccProtoV6ProviderFactories = map[string]func() (
+	tfprotov6.ProviderServer, error,
+){
+	"hpe": newProviderWithError,
+}
+
+func TestAccMorpheusHpeMorpheusScriptTemplateExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := testhelpers.RenderExample(t, "hpe_morpheus_script_template_resource.tf.tmpl",
+		"Name", name,
+		"Labels", "[\"demo\", \"template\", \"terraform\"]",
+		"ScriptType", "bash",
+		"ScriptPhase", "provision",
+		"ScriptContent", "echo \"testing\"",
+		"RunAsUser", "root",
+		"Sudo", "true",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"labels.#",
+			"3",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"labels.0",
+			"demo",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"labels.1",
+			"template",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"labels.2",
+			"terraform",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"script_type",
+			"bash",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"script_phase",
+			"provision",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"script_content",
+			"echo \"testing\"\n",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"run_as_user",
+			"root",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_script_template.tfexample_script_template",
+			"sudo",
+			"true",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Plan
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			// Apply
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			// Plan after apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
