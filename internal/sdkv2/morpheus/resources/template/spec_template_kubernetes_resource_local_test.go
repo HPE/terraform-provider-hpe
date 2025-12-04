dcaa84cc5c5e1614865f1f4aa5bc418e3d217437
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-04 18:05:33 +0000

    Parity: spec_template_kubernetes tests

---
+++ internal/sdkv2/morpheus/resources/template/spec_template_kubernetes_resource_local_test.go
@@ -0,0 +1,101 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+)
+
+func TestAccMorpheusSpecTemplateKubernetesResourceLocalExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	specContent := `---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deployment
+  labels:
+    app: nginx
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - name: nginx
+        image: nginx:1.14.2
+        ports:
+        - containerPort: 80`
+
+	resourceConfig, err := testhelpers.RenderExample(t, "spec_template_kubernetes_resource_local.tf.tmpl",
+		"Name", name,
+		"SourceType", "local",
+		"SpecContent", specContent,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_kubernetes.tfexample_kubernetes_spec_template_local",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_kubernetes.tfexample_kubernetes_spec_template_local",
+			"source_type",
+			"local",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_kubernetes.tfexample_kubernetes_spec_template_local",
+			"spec_content",
+			specContent,
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Plan
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			// Apply
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			// Plan after apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
---
+++ internal/sdkv2/morpheus/resources/template/spec_template_kubernetes_resource_url_test.go
@@ -0,0 +1,78 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+)
+
+func TestAccMorpheusSpecTemplateKubernetesResourceUrlExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := testhelpers.RenderExample(t, "spec_template_kubernetes_resource_url.tf.tmpl",
+		"Name", name,
+		"SourceType", "url",
+		"SpecPath", "http://example.com/spec.yaml",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_kubernetes.tfexample_kubernetes_spec_template_url",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_kubernetes.tfexample_kubernetes_spec_template_url",
+			"source_type",
+			"url",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_kubernetes.tfexample_kubernetes_spec_template_url",
+			"spec_path",
+			"http://example.com/spec.yaml",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Plan
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			// Apply
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			// Plan after apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
