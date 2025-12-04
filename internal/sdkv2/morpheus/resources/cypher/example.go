18ab982f78e366618e022e46351fc5638d1ef019
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-04 16:01:28 +0000

    Parity: hpe_morpheus_cypher_secret tests

---
+++ internal/sdkv2/morpheus/resources/cypher/example.go
@@ -0,0 +1,5 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package cypher
+
+//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_hpe_morpheus_cypher_secret/resource.tf hpe_morpheus_cypher_secret_resource.tf.tmpl Key apipassword Value password123 Ttl 86400
