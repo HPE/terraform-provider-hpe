3fd939d5be1c8a61fbcb166c095d3c0649a47921
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-04 17:25:45 +0000

    remove HpeMorpheus from test name

--- internal/sdkv2/morpheus/resources/blueprint/hpe_morpheus_instance_type_resource_test.go
+++ internal/sdkv2/morpheus/resources/blueprint/hpe_morpheus_instance_type_resource_test.go
@@ -34,7 +34,7 @@ var testAccProtoV6ProviderFactories = map[string]func() (
 	"hpe": newProviderWithError,
 }
 
-func TestAccMorpheusHpeMorpheusInstanceTypeExampleOk(t *testing.T) {
+func TestAccMorpheusInstanceTypeExampleOk(t *testing.T) {
 	t.Parallel()
 
 	defer testhelpers.RecordResult(t)
