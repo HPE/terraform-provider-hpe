2a7d7df362ca7ba11f16eac2548db7ea0916d67d
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-05 12:29:00 +0000

    Parity: spec_template_terraform tests

---
+++ internal/sdkv2/morpheus/resources/template/spec_template_terraform_resource_git_test.go
@@ -0,0 +1,87 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+)
+
+func TestAccMorpheusSpecTemplateTerraformResourceGitExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := testhelpers.RenderExample(t, "spec_template_terraform_resource_git.tf.tmpl",
+		"Name", name,
+		"SourceType", "repository",
+		"RepositoryId", "2",
+		"VersionRef", "main",
+		"SpecPath", "Instance Types/Terraform/CloudResource/aws/vpc.tf",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_git",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_git",
+			"source_type",
+			"repository",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_git",
+			"repository_id",
+			"2",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_git",
+			"version_ref",
+			"main",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_git",
+			"spec_path",
+			"Instance Types/Terraform/CloudResource/aws/vpc.tf",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
---
+++ internal/sdkv2/morpheus/resources/template/spec_template_terraform_resource_url_test.go
@@ -0,0 +1,98 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func newProviderWithError() (tfprotov6.ProviderServer, error) {
+	return tf5to6server.UpgradeServer(context.Background(), sdkv2morpheus.Provider().GRPCProvider)
+}
+
+var testAccProtoV6ProviderFactories = map[string]func() (
+	tfprotov6.ProviderServer, error,
+){
+	"hpe": newProviderWithError,
+}
+
+func TestAccMorpheusSpecTemplateTerraformResourceUrlExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := testhelpers.RenderExample(t, "spec_template_terraform_resource_url.tf.tmpl",
+		"Name", name,
+		"SourceType", "url",
+		"SpecPath", "http://example.com/spec.tf",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_url",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_url",
+			"source_type",
+			"url",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_url",
+			"spec_path",
+			"http://example.com/spec.tf",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
