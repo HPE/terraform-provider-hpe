170d08793b54bd88db68f069e9af3b932fdd934c
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2025-12-09 13:20:42 +0000

    Parity: morpheus_license - add acceptance tests (#683)
    Co-authored-by: Eamonn O'Toole <eamonn.otoole@hpe.com>

---
+++ internal/sdkv2/morpheus/resources/license/morpheus_license_resource_example.go
@@ -0,0 +1,5 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package license
+
+//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_license/resource.tf morpheus_license_resource.tf.tmpl Key 22324FEF3WMCDMMSWE
