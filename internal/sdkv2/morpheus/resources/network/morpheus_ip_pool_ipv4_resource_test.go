a9391aa550c36543a69e7521ac49a63349b31908
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2025-12-09 13:23:12 +0000

    Parity: morpheus_ip_pool_ipv4 - add acceptance tests (#682)
    Co-authored-by: Eamonn O'Toole <eamonn.otoole@hpe.com>

---
+++ internal/sdkv2/morpheus/resources/network/morpheus_ip_pool_ipv4_resource_test.go
@@ -0,0 +1,134 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package network_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+)
+
+// RenderIPPoolIPv4Config generates a Terraform configuration for hpe_morpheus_ip_pool_ipv4 resource.
+// It accepts an optional map of field overrides. If nil or empty, default values are used.
+func RenderIPPoolIPv4Config(t *testing.T, overrides map[string]string) (string, error) {
+	t.Helper()
+
+	defaults := map[string]string{
+		"Name":             "\"" + acctest.RandomWithPrefix(t.Name()) + "\"",
+		"StartingAddress1": "\"192.168.1.1\"",
+		"EndingAddress1":   "\"192.168.1.10\"",
+		"StartingAddress2": "\"10.0.0.1\"",
+		"EndingAddress2":   "\"10.0.0.10\"",
+	}
+
+	for key, value := range overrides {
+		defaults[key] = value
+	}
+
+	args := make([]string, 0, len(defaults)*2)
+	for key, value := range defaults {
+		args = append(args, key, value)
+	}
+
+	return testhelpers.RenderExample(t, "morpheus_ip_pool_ipv4_resource.tf.tmpl", args...)
+}
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func newProviderWithError() (tfprotov6.ProviderServer, error) {
+	return tf5to6server.UpgradeServer(context.Background(), sdkv2morpheus.Provider().GRPCProvider)
+}
+
+var testAccProtoV6ProviderFactories = map[string]func() (
+	tfprotov6.ProviderServer, error,
+){
+	"hpe": newProviderWithError,
+}
+
+func TestAccMorpheusIpPoolIpv4ResourceExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := RenderIPPoolIPv4Config(t, map[string]string{
+		"Name": "\"" + name + "\"",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_ip_pool_ipv4.tf_example_ipv4_pool",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_ip_pool_ipv4.tf_example_ipv4_pool",
+			"ip_range.0.starting_address",
+			"192.168.1.1",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_ip_pool_ipv4.tf_example_ipv4_pool",
+			"ip_range.0.ending_address",
+			"192.168.1.10",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_ip_pool_ipv4.tf_example_ipv4_pool",
+			"ip_range.1.starting_address",
+			"10.0.0.1",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_ip_pool_ipv4.tf_example_ipv4_pool",
+			"ip_range.1.ending_address",
+			"10.0.0.10",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Plan
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			// Apply
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			// Plan after apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
