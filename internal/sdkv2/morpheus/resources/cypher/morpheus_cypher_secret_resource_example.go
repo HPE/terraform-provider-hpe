4fd6ce72e003aa044c40520b1ff0229d67ce2169
Jason Quigley <jason.quigley@hpe.com>
2025-12-12 15:33:28 +0000

    fix test

--- internal/sdkv2/morpheus/resources/cypher/morpheus_cypher_secret_resource_example.go
+++ internal/sdkv2/morpheus/resources/cypher/morpheus_cypher_secret_resource_example.go
@@ -2,4 +2,4 @@
 
 package cypher
 
-//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_hpe_morpheus_cypher_secret/resource.tf hpe_morpheus_cypher_secret_resource.tf.tmpl Key apipassword Value password123 Ttl 86400
+//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_cypher_secret/resource.tf morpheus_cypher_secret_resource_tf.tmpl Key apipassword Value password123 Ttl 86400
