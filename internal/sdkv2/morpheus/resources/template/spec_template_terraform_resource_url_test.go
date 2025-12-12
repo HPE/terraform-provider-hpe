a002494c7b010e9d2603079c0f3cd30cc8002a97
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-12 18:50:36 +0000

    Parity: spec_template_terraform tests (#697)
    Adds acceptance test(s) for the resource spec_template_terraform.
    
    Also adds template files for the example Terraform config with a
    go:generate statement.
    
    ---------
    
    Co-authored-by: Jason Quigley <jason.quigley@hpe.com>
    Co-authored-by: Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>

---
+++ internal/sdkv2/morpheus/resources/template/spec_template_terraform_resource_url_test.go
@@ -0,0 +1,102 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+)
+
+// RenderSpecTemplateTerraformUrlConfig renders the Terraform config for
+// spec_template_terraform_resource_url tests
+func RenderSpecTemplateTerraformUrlConfig(
+	t *testing.T,
+	name string,
+	overrides map[string]string,
+) (string, error) {
+	t.Helper()
+
+	defaults := map[string]string{
+		"Name":       name,
+		"SourceType": "url",
+		"SpecPath":   "http://example.com/spec.tf",
+	}
+
+	for key, value := range overrides {
+		defaults[key] = value
+	}
+
+	args := []string{}
+	for key, value := range defaults {
+		args = append(args, key, value)
+	}
+
+	return testhelpers.RenderExample(
+		t,
+		"morpheus_spec_template_terraform_resource_url.tf.tmpl",
+		args...,
+	)
+}
+
+func TestAccMorpheusSpecTemplateTerraformResourceUrlExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := RenderSpecTemplateTerraformUrlConfig(t, name, map[string]string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_url",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_url",
+			"source_type",
+			"url",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_spec_template_terraform.tfexample_terraform_spec_terraform_url",
+			"spec_path",
+			"http://example.com/spec.tf",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
