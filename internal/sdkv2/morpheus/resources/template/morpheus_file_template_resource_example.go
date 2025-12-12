73d9a1b1d6a545bdc9a556e5c75e1d775f02df39
Jason Quigley <jason.quigley@hpe.com>
2025-12-12 17:11:29 +0000

    export render function

--- internal/sdkv2/morpheus/resources/template/morpheus_file_template_resource_example.go
+++ internal/sdkv2/morpheus/resources/template/morpheus_file_template_resource_example.go
@@ -2,4 +2,4 @@
 
 package template
 
-//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_file_template/resource.tf hpe_morpheus_file_template_resource.tf.tmpl Name tf-terraform-file-template Labels ["demo","template","terraform"] FileName tfcustom.cnf FilePath /etc/my.cnf.d Phase preProvision FileOwner root SettingName myCnf SettingCategory master
+//go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_file_template/resource.tf morpheus_file_template_resource.tf.tmpl Name tf-terraform-file-template Labels ["demo","template","terraform"] FileName tfcustom.cnf FilePath /etc/my.cnf.d Phase preProvision FileOwner root SettingName myCnf SettingCategory master
