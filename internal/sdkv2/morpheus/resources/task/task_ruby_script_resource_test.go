eca8fa5a47406fdadd0ee85f15207d26576cafd2
Jason Quigley <jason.quigley@hpe.com>
2025-12-12 19:30:28 +0000

    update tests and examples

--- internal/sdkv2/morpheus/resources/task/task_ruby_script_resource_test.go
+++ internal/sdkv2/morpheus/resources/task/task_ruby_script_resource_test.go
@@ -3,36 +3,13 @@
 package task_test
 
 import (
-	"context"
-	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 
 	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
-	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
 )
-
-func TestMain(m *testing.M) {
-	code := m.Run()
-
-	testhelpers.WriteMergedResults()
-
-	os.Exit(code)
-}
-
-func newProviderWithError() (tfprotov6.ProviderServer, error) {
-	return tf5to6server.UpgradeServer(context.Background(), sdkv2morpheus.Provider().GRPCProvider)
-}
-
-var testAccProtoV6ProviderFactories = map[string]func() (
-	tfprotov6.ProviderServer, error,
-){
-	"hpe": newProviderWithError,
-}
 
 func RenderTaskRubyScriptConfig(
 	t *testing.T,
