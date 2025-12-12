47be782be834383d9aca7042e02c7b2e2cd7f56d
Jason Quigley <jason.quigley@hpe.com>
2025-12-12 17:12:58 +0000

    update tests and examples

---
+++ internal/sdkv2/morpheus/resources/template/morpheus_file_template_resource_test.go
@@ -0,0 +1,155 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package template_test
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-mux/tf5to6server"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func newProviderWithError() (tfprotov6.ProviderServer, error) {
+	return tf5to6server.UpgradeServer(context.Background(), sdkv2morpheus.Provider().GRPCProvider)
+}
+
+var testAccProtoV6ProviderFactories = map[string]func() (
+	tfprotov6.ProviderServer, error,
+){
+	"hpe": newProviderWithError,
+}
+
+func RenderHpeMorpheusFileTemplateConfig(
+	t *testing.T,
+	name string,
+	overrides map[string]string,
+) (string, error) {
+	t.Helper()
+
+	defaults := map[string]string{
+		"Name":            name,
+		"Labels":          `["demo", "template", "terraform"]`,
+		"FileName":        "tfcustom.cnf",
+		"FilePath":        "/etc/my.cnf.d",
+		"Phase":           "preProvision",
+		"FileContent":     `"# Test MySQL Configuration\n[mysqld]\ninnodb_buffer_pool_size = 128M"`,
+		"FileOwner":       "root",
+		"SettingName":     "myCnf",
+		"SettingCategory": "master",
+	}
+
+	for key, value := range overrides {
+		defaults[key] = value
+	}
+
+	return testhelpers.RenderExample(
+		t,
+		"morpheus_file_template_resource.tf.tmpl",
+		"Name", defaults["Name"],
+		"Labels", defaults["Labels"],
+		"FileName", defaults["FileName"],
+		"FilePath", defaults["FilePath"],
+		"Phase", defaults["Phase"],
+		"FileContent", defaults["FileContent"],
+		"FileOwner", defaults["FileOwner"],
+		"SettingName", defaults["SettingName"],
+		"SettingCategory", defaults["SettingCategory"],
+	)
+}
+
+func TestAccMorpheusFileTemplateExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := RenderHpeMorpheusFileTemplateConfig(t, name, map[string]string{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_file_template.tfexample_file_template",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_file_template.tfexample_file_template",
+			"file_name",
+			"tfcustom.cnf",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_file_template.tfexample_file_template",
+			"file_path",
+			"/etc/my.cnf.d",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_file_template.tfexample_file_template",
+			"phase",
+			"preProvision",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_file_template.tfexample_file_template",
+			"file_owner",
+			"root",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_file_template.tfexample_file_template",
+			"setting_name",
+			"myCnf",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_file_template.tfexample_file_template",
+			"setting_category",
+			"master",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Plan
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: true,
+				Check:              checkFn,
+				PlanOnly:           true,
+			},
+			// Apply
+			{
+				Config: providerConfig + resourceConfig,
+				Check:  checkFn,
+			},
+			// Plan after apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
