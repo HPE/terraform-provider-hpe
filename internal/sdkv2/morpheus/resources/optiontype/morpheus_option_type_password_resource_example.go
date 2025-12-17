d20b4f0eb42f3853316f040b4cee0a90c6c86a4e
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2025-12-17 13:15:40 +0000

    fix build failure

--- internal/sdkv2/morpheus/resources/optiontype/morpheus_option_type_password_resource_example.go
+++ internal/sdkv2/morpheus/resources/optiontype/morpheus_option_type_password_resource_example.go
@@ -3,13 +3,12 @@
 package optiontype
 
 import (
-	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
-
 	"fmt"
-	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
 	"path/filepath"
 	"runtime"
 	"testing"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
 )
 
 //go:generate go run ../../../../../cmd/render -out examples/resources/morpheus_option_type_password/resource.tf morpheus_option_type_password_resource.tf.tmpl Name tf_example_password_option_type Description "Terraform password option type example" Labels "[\"demo\", \"terraform\"]" FieldName tfPasswordExample ExportMeta true DependentField dependent_example VisibilityField visibility_example RequireField require_example ShowOnEdit true Editable true DisplayValueOnDetails true FieldLabel numbers Placeholder fewf DefaultValue testing HelpBlock fiwefw Required true VerifyPattern "a\\\\D{4}"
