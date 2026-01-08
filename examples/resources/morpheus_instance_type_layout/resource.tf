f938522cf4bd3149412e272688cd1ca47b1ae719
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2026-01-08 15:21:17 +0000

    sdkv2: Add tests and example generation for instance type layout

--- examples/resources/morpheus_instance_type_layout/resource.tf
+++ examples/resources/morpheus_instance_type_layout/resource.tf
@@ -1,11 +1,7 @@
 resource "hpe_morpheus_instance_type_layout" "tf_example_instance_layout" {
-  instance_type_id = morpheus_instance_type.tf_example_instance_type.id
+  instance_type_id = hpe_morpheus_instance_type.tf_example_instance_type.id
   name             = "todo_app_frontend"
   labels           = ["demo", "layout", "terraform"]
   version          = "1.0"
   technology       = "vmware"
-  node_type_ids = [
-    morpheus_node_type.ubuntu_base.id
-  ]
-  workflow_id = morpheus_provisioning_workflow.tfexample_workflow.id
 }
