cc1a8cf1a5237243bea71203c46db797be158c8e
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2026-01-08 15:31:29 +0000

    regen docs

--- examples/resources/morpheus_instance_type_layout/resource.tf
+++ examples/resources/morpheus_instance_type_layout/resource.tf
@@ -1,4 +1,4 @@
-resource "hpe_morpheus_instance_type_layout" "tf_example_instance_layout" {
+resource "hpe_morpheus_instance_type_layout" "example" {
   instance_type_id = hpe_morpheus_instance_type.tf_example_instance_type.id
   name             = "todo_app_frontend"
   labels           = ["demo", "layout", "terraform"]
