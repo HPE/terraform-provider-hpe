5a85b62d650711d39b5096c90e0105ceab344154
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2026-01-09 11:26:56 +0000

    Parity: workflow_operational tests

---
+++ internal/sdkv2/morpheus/resources/workflow/workflow_operational_test.go
@@ -0,0 +1,96 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package workflow_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/workflow"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func TestAccMorpheusWorkflowOperationalExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	resourceConfig, err := workflow.RenderWorkflowOperationalConfig(t, map[string]string{
+		"Name": name,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_workflow_operational.example",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_workflow_operational.example",
+			"description",
+			"Terraform operational workflow example",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_workflow_operational.example",
+			"labels.#",
+			"2",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_workflow_operational.example",
+			"platform",
+			"all",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_workflow_operational.example",
+			"visibility",
+			"private",
+		),
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_workflow_operational.example",
+			"allow_custom_config",
+			"true",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testhelpers.GetAccTestFactories(t, nil, sdkv2morpheus.Provider()),
+		Steps: []resource.TestStep{
+			// Apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				Check:              checkFn,
+			},
+			// Plan after apply
+			{
+				Config:             providerConfig + resourceConfig,
+				ExpectNonEmptyPlan: false,
+				PlanOnly:           true,
+			},
+		},
+	})
+}
