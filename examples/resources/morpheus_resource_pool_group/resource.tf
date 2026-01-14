3ce57c9ba9f790237dcb83bc0d5fc3896415cf44
matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
2026-01-14 13:18:46 +0000

    Parity: resource_pool_group tests (#778)
    Adds test and generated example code for
    `hpe_morpheus_resource_pool_group`.
    
    Note we're skipping the test for now - we're lacking resource pools
    which we can create per-test.
    
    Co-authored-by: Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>

--- examples/resources/morpheus_resource_pool_group/resource.tf
+++ examples/resources/morpheus_resource_pool_group/resource.tf
@@ -1,4 +1,4 @@
-resource "hpe_morpheus_resource_pool_group" "tfexample_resource_pool_group" {
+resource "hpe_morpheus_resource_pool_group" "example" {
   name              = "TFExample Resource Pool Group"
   description       = "TFExample Resource Pool Group"
   mode              = "roundRobin"
