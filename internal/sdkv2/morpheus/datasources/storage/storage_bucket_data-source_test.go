b9629c51bc1feecf888fd3159de1ed9d675aef7c
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2026-01-15 15:24:53 +0000

    Parity: Add storage_bucket data source tests

---
+++ internal/sdkv2/morpheus/datasources/storage/storage_bucket_data-source_test.go
@@ -0,0 +1,73 @@
+// (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
+
+package storage_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus"
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+	dsstorage "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/datasources/storage"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func TestAccMorpheusDataSourceStorageBucketExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	t.Skip("Skipping due to missing resource implementation")
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	var dependenciesConfig string
+
+	datasourceConfig, err := dsstorage.RenderStorageBucketConfig(t, map[string]string{
+		"Name": name,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"data.hpe_morpheus_storage_bucket.example",
+			"name",
+			name,
+		),
+		resource.TestCheckResourceAttrSet(
+			"data.hpe_morpheus_storage_bucket.example",
+			"id",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testhelpers.GetAccTestFactories(t, morpheus.New(), sdkv2morpheus.Provider()),
+		Steps: []resource.TestStep{
+			{
+				Config:             providerConfig + dependenciesConfig + datasourceConfig,
+				ExpectNonEmptyPlan: false,
+				Check:              checkFn,
+			},
+		},
+	})
+}
