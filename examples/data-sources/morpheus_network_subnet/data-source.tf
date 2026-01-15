8676f65d6f52b1d5e9c607ce7681e675baeb9203
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2026-01-15 18:01:39 +0000

    Parity: Add network_subnet data source tests (#812)

--- examples/data-sources/morpheus_network_subnet/data-source.tf
+++ examples/data-sources/morpheus_network_subnet/data-source.tf
@@ -1,4 +1,4 @@
-data "hpe_morpheus_network_subnet" "tf_example_network_subnet" {
+data "hpe_morpheus_network_subnet" "example" {
   network_id = 166333
   name       = "TF Example Network Subnet"
 }
