57cdb02f5f9ce7787f991ce703745bc0cc41e22c
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2026-01-15 16:48:34 +0000

    Parity: Add job data source tests (#808)

---
+++ internal/sdkv2/morpheus/datasources/job/job_data-source_test.go
@@ -0,0 +1,89 @@
+// (C) Copyright 2025-2026 Hewlett Packard Enterprise Development LP
+
+package job_test
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus"
+	"github.com/HPE/terraform-provider-hpe/internal/framework/subproviders/morpheus/testhelpers"
+	sdkv2morpheus "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus"
+	dsjob "github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/datasources/job"
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/job"
+	"github.com/HPE/terraform-provider-hpe/internal/sdkv2/morpheus/resources/task"
+)
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	testhelpers.WriteMergedResults()
+
+	os.Exit(code)
+}
+
+func TestAccMorpheusDataSourceJobExampleOk(t *testing.T) {
+	t.Parallel()
+
+	defer testhelpers.RecordResult(t)
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	providerConfig := testhelpers.ProviderBlock()
+
+	name := acctest.RandomWithPrefix(t.Name())
+
+	var dependenciesConfig string
+
+	if currentDependency, err := task.RenderTaskEmailConfig(t, map[string]string{
+		"Name": name,
+		"Code": strings.ToLower(name),
+	}); err != nil {
+		t.Fatal(err)
+	} else {
+		dependenciesConfig += currentDependency
+	}
+
+	if currentDependency, err := job.RenderJobTaskManualConfig(t, map[string]string{
+		"Name":   name,
+		"Code":   strings.ToLower(name),
+		"TaskId": "resource.hpe_morpheus_task_email.tfexample_email.id",
+	}); err != nil {
+		t.Fatal(err)
+	} else {
+		dependenciesConfig += currentDependency
+	}
+
+	datasourceConfig, err := dsjob.RenderJobConfig(t, map[string]string{
+		"Name": "resource.hpe_morpheus_job_task.example.name",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttr(
+			"data.hpe_morpheus_job.example",
+			"name",
+			name,
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testhelpers.GetAccTestFactories(t, morpheus.New(), sdkv2morpheus.Provider()),
+		Steps: []resource.TestStep{
+			{
+				Config:             providerConfig + dependenciesConfig + datasourceConfig,
+				ExpectNonEmptyPlan: false,
+				Check:              checkFn,
+			},
+		},
+	})
+}
