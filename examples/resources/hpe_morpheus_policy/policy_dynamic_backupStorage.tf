4f9806f7ef59f39d1f13e1b98903072a7826b223
RDavisHPE <135722327+RDavisHPE@users.noreply.github.com>
2026-01-23 13:30:05 +0000

    Documentation for Backup Targets (backupStorage) support in policy (#859)
    Updating documentation to reflect that Backup Targets (backupStorage) is
    now supported in the hpe_morpheus_policy resource for Morpheus 8.1.0 or
    later. This was previously marked as unsupported (despite being
    implemented) due to an API bug that has since been resolved as of
    Morpheus 8.1.0.
    - Updated CHANGELOG.md and index.md.tmpl to remove the known issue.
    - Updated morpheus_policy.md.tmpl to indicate support for Backup Targets
    (backupStorage) and included (and uncommented) example configuration
    file.
    - Ran make docs

--- examples/resources/hpe_morpheus_policy/policy_dynamic_backupStorage.tf
+++ examples/resources/hpe_morpheus_policy/policy_dynamic_backupStorage.tf
@@ -1,19 +1,20 @@
-# # Backup Targets Policy - Restricts available backup storage targets
+# Backup Targets Policy - Restricts available backup storage targets
 # Allowed associated_resource_types: Group, Cloud, User, Global, Role
 # Tenant specification: NOT allowed (cannot specify tenants array)
-# resource "hpe_morpheus_policy" "backup_targets" {
-#   name                     = "Backup Targets Policy"
-#   description              = "Restrict available backup targets"
-#   associated_resource_type = "User"
-#   associated_resource_id   = 9969
-#   enabled                  = true
-#
-#   policy_type = {
-#     code = "backupStorage"
-#   }
-#
-#   config = {
-#     # Required
-#     backupStorageIds = [5, 6] # Array of backup storage IDs to restrict available backup targets
-#   }
-# }
+# Supported in Morpheus 8.1.0 or later (previous versions double nest the backupStorageIds attribute)
+resource "hpe_morpheus_policy" "backup_targets" {
+  name                     = "Backup Targets Policy"
+  description              = "Restrict available backup targets"
+  associated_resource_type = "User"
+  associated_resource_id   = 9969
+  enabled                  = true
+
+  policy_type = {
+    code = "backupStorage"
+  }
+
+  config = {
+    # Required
+    backupStorageIds = [5, 6] # Array of backup storage IDs to restrict available backup targets
+  }
+}
