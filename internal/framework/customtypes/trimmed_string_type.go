13daab0b2e2ac7b8b17581793585872f3c4ee5d6
ZoeySimone <78411653+ZoeySimone@users.noreply.github.com>
2026-01-30 11:53:01 +0000

    Add a custom type for TrimmedStrings (#875)
    Adds a new custom type which ignores the differences in leading and
    trailing whitespace in String attributes.
    
    The custom type implements the
    `basetypes.StringValuableWithSemanticEquals` interface which
    allows implementing custom logic for semantic equality. In this case the
    implementation uses
    `strings.TrimSpace()` to avoid drift detection where an API may have
    trimmed whitespace from
    the input.

---
+++ internal/framework/customtypes/trimmed_string_type.go
@@ -0,0 +1,72 @@
+package customtypes
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+	"github.com/hashicorp/terraform-plugin-go/tftypes"
+)
+
+var _ basetypes.StringTypable = (*TrimmedStringType)(nil)
+
+// TrimmedStringType is a custom String type which ignores differences in
+// leading and trailing whitespace
+type TrimmedStringType struct {
+	basetypes.StringType
+}
+
+// String returns a human readable string of the type name.
+func (t TrimmedStringType) String() string {
+	return "customtypes.TrimmedStringType"
+}
+
+// ValueType returns the Value type.
+func (t TrimmedStringType) ValueType(ctx context.Context) attr.Value {
+	return TrimmedString{}
+}
+
+// Equal returns true if the given type is equivalent.
+func (t TrimmedStringType) Equal(o attr.Type) bool {
+	other, ok := o.(TrimmedStringType)
+
+	if !ok {
+		return false
+	}
+
+	return t.StringType.Equal(other.StringType)
+}
+
+// ValueFromString returns a StringValuable type given a StringValue.
+func (t TrimmedStringType) ValueFromString(
+	ctx context.Context,
+	in basetypes.StringValue,
+) (basetypes.StringValuable, diag.Diagnostics) {
+	return TrimmedString{
+		StringValue: in,
+	}, nil
+}
+
+// ValueFromTerraform returns a Value given a tftypes.Value.  This is meant to
+// convert the tftypes.Value into a more convenient Go type for the provider to
+// consume the data with.
+func (t TrimmedStringType) ValueFromTerraform(ctx context.Context, in tftypes.Value) (attr.Value, error) {
+	attrValue, err := t.StringType.ValueFromTerraform(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+
+	stringValue, ok := attrValue.(basetypes.StringValue)
+	if !ok {
+		return nil, fmt.Errorf("unexpected value type of %T", attrValue)
+	}
+
+	stringValuable, diags := t.ValueFromString(ctx, stringValue)
+	if diags.HasError() {
+		return nil, fmt.Errorf("unexpected error converting StringValue to StringValuable: %v", diags)
+	}
+
+	return stringValuable, nil
+}
---
+++ internal/framework/customtypes/trimmed_string_value_test.go
@@ -0,0 +1,114 @@
+package customtypes_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+
+	"github.com/HPE/terraform-provider-hpe/internal/framework/customtypes"
+)
+
+func TestNewTrimmedStringValue(t *testing.T) {
+	tests := []struct {
+		name  string
+		value string
+		want  customtypes.TrimmedString
+	}{
+		{
+			name:  "NormalString",
+			value: "test",
+			want: customtypes.TrimmedString{
+				StringValue: basetypes.NewStringValue("test"),
+			},
+		},
+		{
+			name:  "MultilineString",
+			value: "\nnewline\ntest\n",
+			want: customtypes.TrimmedString{
+				StringValue: basetypes.NewStringValue("\nnewline\ntest\n"),
+			},
+		},
+		{
+			name:  "EmptyString",
+			value: "",
+			want: customtypes.TrimmedString{
+				StringValue: basetypes.NewStringValue(""),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := customtypes.NewTrimmedStringValue(tt.value)
+			if !got.Equal(tt.want) {
+				t.Errorf("NewTrimmedStringValue() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTrimmedString_StringSemanticEquals(t *testing.T) {
+	tests := []struct {
+		name        string
+		value       customtypes.TrimmedString
+		newValuable basetypes.StringValuable
+		want        bool
+		wantErr     bool
+	}{
+		{
+			name:        "Newlines",
+			value:       customtypes.NewTrimmedStringValue("\n\n\nnewline\ntest\n\n\n"),
+			newValuable: customtypes.NewTrimmedStringValue("newline\ntest"),
+			want:        true,
+			wantErr:     false,
+		},
+		{
+			name:        "ReturnNewline",
+			value:       customtypes.NewTrimmedStringValue("\r\nnewline\r\ntest\r\n\r\n"),
+			newValuable: customtypes.NewTrimmedStringValue("newline\r\ntest"),
+			want:        true,
+			wantErr:     false,
+		},
+		{
+			name:        "Spaces",
+			value:       customtypes.NewTrimmedStringValue("  newline\r\ntest  "),
+			newValuable: customtypes.NewTrimmedStringValue("newline\r\ntest"),
+			want:        true,
+			wantErr:     false,
+		},
+		{
+			name:        "NoWhitespace",
+			value:       customtypes.NewTrimmedStringValue("newline\ntest"),
+			newValuable: customtypes.NewTrimmedStringValue("newline\ntest"),
+			want:        true,
+			wantErr:     false,
+		},
+		{
+			name:        "StringValue",
+			value:       customtypes.NewTrimmedStringValue("newline\ntest"),
+			newValuable: basetypes.NewStringValue("test"),
+			want:        false,
+			wantErr:     true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, gotErr := tt.value.StringSemanticEquals(context.Background(), tt.newValuable)
+			if gotErr != nil {
+				if !tt.wantErr {
+					t.Errorf("StringSemanticEquals() failed: %v", gotErr)
+				}
+
+				return
+			}
+
+			if tt.wantErr {
+				t.Fatal("StringSemanticEquals() succeeded unexpectedly")
+			}
+
+			if got != tt.want {
+				t.Errorf("StringSemanticEquals() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
