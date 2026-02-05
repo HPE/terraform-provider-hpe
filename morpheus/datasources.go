6e8baf51d7898f77ea65c5e6084195eaaddccb3b
ZoeySimone <78411653+ZoeySimone@users.noreply.github.com>
2026-02-05 15:35:10 +0000

    Remove experimental build tag (#886)
    We adopted an approach to releasing where we don't use the experimental
    build tag anymore.

--- morpheus/datasources.go
+++ morpheus/datasources.go
@@ -1,16 +1,5 @@
 // (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 
-//go:build !experimental
-
-// This file is used to include the Morpheus subprovider datasources in the
-// release build. It is not used in the experimental build. It is used to
-// include only the stable datasources in the release build. When building the
-// experimental version, use the `-tags experimental` flag to exclude this
-// file.
-
-// When datasources are ready for production use, they should be moved to this
-// file
-
 package morpheus
 
 import (
