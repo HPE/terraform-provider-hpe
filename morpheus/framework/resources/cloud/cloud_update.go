053f30ebaf608943f6b06bf99180d93eef6d4cd3
Rory McEvoy <rory.mcevoy@hpe.com>
2026-03-09 15:47:46 +0000

    Cloud AWS update opertation

--- morpheus/framework/resources/cloud/cloud_update.go
+++ morpheus/framework/resources/cloud/cloud_update.go
@@ -125,6 +125,137 @@ func (r *Resource) Update(
 	}
 
 	switch {
+	case !plan.ConfigAws.IsNull() && !plan.ConfigAws.IsUnknown():
+		if !plan.ConfigAws.AccessKey.IsNull() && !plan.ConfigAws.AccessKey.IsUnknown() {
+			updateCloud.Config["accessKey"] = plan.ConfigAws.AccessKey.ValueString()
+		}
+
+		if !plan.ConfigAws.ApiProxy.IsNull() && !plan.ConfigAws.ApiProxy.IsUnknown() {
+			updateCloud.Config["apiProxy"] = plan.ConfigAws.ApiProxy.ValueString()
+		}
+
+		if !plan.ConfigAws.BackupProvider.IsNull() && !plan.ConfigAws.BackupProvider.IsUnknown() {
+			updateCloud.Config["backupMode"] = plan.ConfigAws.BackupProvider.ValueString()
+		}
+
+		if !plan.ConfigAws.BypassProxy.IsNull() && !plan.ConfigAws.BypassProxy.IsUnknown() {
+			updateCloud.Config["bypassProxyForCloud"] = convert.
+				BoolTypeToStringPointerOnOff(plan.ConfigAws.BypassProxy)
+		}
+
+		if !plan.ConfigAws.ChangeManagementConfig.IsNull() && !plan.ConfigAws.ChangeManagementConfig.IsUnknown() {
+			updateCloud.Config["changeManagementConfig"] = plan.ConfigAws.ChangeManagementConfig.ValueString()
+		}
+
+		if !plan.ConfigAws.CmdbConfig.IsNull() && !plan.ConfigAws.CmdbConfig.IsUnknown() {
+			updateCloud.Config["cmdbConfig"] = plan.ConfigAws.CmdbConfig.ValueString()
+		}
+
+		if !plan.ConfigAws.CmdbDiscovery.IsNull() && !plan.ConfigAws.CmdbDiscovery.IsUnknown() {
+			updateCloud.Config["configCmdbDiscovery"] = convert.
+				BoolTypeToStringPointerOnOff(plan.ConfigAws.CmdbDiscovery)
+		}
+
+		if !plan.ConfigAws.ConfigManagementId.IsNull() && !plan.ConfigAws.ConfigManagementId.IsUnknown() {
+			updateCloud.Config["configManagementId"] = plan.ConfigAws.ConfigManagementId.ValueString()
+		}
+
+		if !plan.ConfigAws.Costing.IsNull() && !plan.ConfigAws.Costing.IsUnknown() {
+			updateCloud.Config["costing"] = plan.ConfigAws.Costing.ValueString()
+		}
+
+		if !plan.ConfigAws.CostingBucket.IsNull() && !plan.ConfigAws.CostingBucket.IsUnknown() {
+			updateCloud.Config["costingBucket"] = plan.ConfigAws.CostingBucket.ValueString()
+		}
+
+		if !plan.ConfigAws.CostingFolder.IsNull() && !plan.ConfigAws.CostingFolder.IsUnknown() {
+			updateCloud.Config["costingFolder"] = plan.ConfigAws.CostingFolder.ValueString()
+		}
+
+		if !plan.ConfigAws.CostingKey.IsNull() && !plan.ConfigAws.CostingKey.IsUnknown() {
+			updateCloud.Config["costingKey"] = plan.ConfigAws.CostingKey.ValueString()
+		}
+
+		if !plan.ConfigAws.CostingReportName.IsNull() && !plan.ConfigAws.CostingReportName.IsUnknown() {
+			updateCloud.Config["costingReportName"] = plan.ConfigAws.CostingReportName.ValueString()
+		}
+
+		if !plan.ConfigAws.CostingSecret.IsNull() && !plan.ConfigAws.CostingSecret.IsUnknown() {
+			updateCloud.Config["costingSecret"] = plan.ConfigAws.CostingSecret.ValueString()
+		}
+
+		if !plan.ConfigAws.Credentials.IsNull() && !plan.ConfigAws.Credentials.IsUnknown() {
+			updateCloud.Config["useHostCredentials"] = plan.ConfigAws.Credentials.ValueString()
+		}
+
+		if !plan.ConfigAws.DarkModeLogo.IsNull() && !plan.ConfigAws.DarkModeLogo.IsUnknown() {
+			updateCloud.Config["darkModeLogo"] = plan.ConfigAws.DarkModeLogo.ValueString()
+		}
+
+		if !plan.ConfigAws.Domain.IsNull() && !plan.ConfigAws.Domain.IsUnknown() {
+			updateCloud.Config["domain"] = plan.ConfigAws.Domain.ValueString()
+		}
+
+		if !plan.ConfigAws.EbsEncryption.IsNull() && !plan.ConfigAws.EbsEncryption.IsUnknown() {
+			updateCloud.Config["ebsEncryption"] = plan.ConfigAws.EbsEncryption.ValueString()
+		}
+
+		if !plan.ConfigAws.Endpoint.IsNull() && !plan.ConfigAws.Endpoint.IsUnknown() {
+			updateCloud.Config["endpoint"] = plan.ConfigAws.Endpoint.ValueString()
+		}
+
+		if !plan.ConfigAws.Guidance.IsNull() && !plan.ConfigAws.Guidance.IsUnknown() {
+			updateCloud.Config["guidance"] = plan.ConfigAws.Guidance.ValueString()
+		}
+
+		if !plan.ConfigAws.Logo.IsNull() && !plan.ConfigAws.Logo.IsUnknown() {
+			updateCloud.Config["logo"] = plan.ConfigAws.Logo.ValueString()
+		}
+
+		if !plan.ConfigAws.NetworkMode.IsNull() && !plan.ConfigAws.NetworkMode.IsUnknown() {
+			updateCloud.Config["networkMode"] = plan.ConfigAws.NetworkMode.ValueString()
+		}
+
+		if !plan.ConfigAws.NoProxy.IsNull() && !plan.ConfigAws.NoProxy.IsUnknown() {
+			updateCloud.Config["noProxy"] = plan.ConfigAws.NoProxy.ValueString()
+		}
+
+		if !plan.ConfigAws.Proxy.IsNull() && !plan.ConfigAws.Proxy.IsUnknown() {
+			updateCloud.Config["proxy"] = plan.ConfigAws.Proxy.ValueString()
+		}
+
+		if !plan.ConfigAws.Region.IsNull() && !plan.ConfigAws.Region.IsUnknown() {
+			updateCloud.Config["region"] = plan.ConfigAws.Region.ValueString()
+		}
+
+		if !plan.ConfigAws.ReplicationProvider.IsNull() && !plan.ConfigAws.ReplicationProvider.IsUnknown() {
+			updateCloud.Config["replicationMode"] = plan.ConfigAws.ReplicationProvider.ValueString()
+		}
+
+		if !plan.ConfigAws.RoleArn.IsNull() && !plan.ConfigAws.RoleArn.IsUnknown() {
+			updateCloud.Config["stsAssumeRole"] = plan.ConfigAws.RoleArn.ValueString()
+		}
+
+		if !plan.ConfigAws.SecretKey.IsNull() && !plan.ConfigAws.SecretKey.IsUnknown() {
+			updateCloud.Config["secretKey"] = plan.ConfigAws.SecretKey.ValueString()
+		}
+
+		if !plan.ConfigAws.Timezone.IsNull() && !plan.ConfigAws.Timezone.IsUnknown() {
+			updateCloud.Config["timezone"] = plan.ConfigAws.Timezone.ValueString()
+		}
+
+		if !plan.ConfigAws.UserData.IsNull() && !plan.ConfigAws.UserData.IsUnknown() {
+			updateCloud.Config["userDataLinux"] = plan.ConfigAws.UserData.ValueString()
+		}
+
+		if !plan.ConfigAws.VdiGateway.IsNull() && !plan.ConfigAws.VdiGateway.IsUnknown() {
+			updateCloud.Config["vdiGateway"] = plan.ConfigAws.VdiGateway.ValueString()
+		}
+
+		if !plan.ConfigAws.Vpc.IsNull() && !plan.ConfigAws.Vpc.IsUnknown() {
+			updateCloud.Config["vpc"] = plan.ConfigAws.Vpc.ValueString()
+		}
+
 	case !plan.ConfigHvm.IsNull() && !plan.ConfigHvm.IsUnknown():
 		if !plan.ConfigHvm.CertificateProvider.IsNull() && !plan.ConfigHvm.CertificateProvider.IsUnknown() {
 			updateCloud.Config["certificateProvider"] = plan.ConfigHvm.CertificateProvider.ValueString()
