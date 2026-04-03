6b795b05ee9cd30a8b1233e088287dc2e784b6d0
Eamonn O'Toole <eamonn.otoole@hpe.com>
2026-04-03 12:41:07 +0100

    Add support for forms group option type
    In this PR we add support for the group forms option type.  We've
    updated docs and acceptance tests. We've also updated the version of the
    legacy SDK.

--- morpheus/sdkv2/resources/form/form_resource_test.go
+++ morpheus/sdkv2/resources/form/form_resource_test.go
@@ -1065,6 +1065,102 @@ func TestAccMorpheusFormExampleOk(t *testing.T) {
 			"option_type8_type",
 			"cloud",
 		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_allow_read_only",
+			"true",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_code",
+			"group-input",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_default_value",
+			"test123",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_description",
+			"Terraform group example",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_display_value_on_details",
+			"true",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_exclude_from_search",
+			"true",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_export_meta",
+			"true",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_field_label",
+			"group input",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_field_name",
+			"groupInput",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_help_block",
+			"Select a group",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_hidden",
+			"false",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_locked",
+			"true",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_name",
+			"tf group example",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_placeholder",
+			"Select group",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_required",
+			"true",
+		),
+
+		resource.TestCheckResourceAttr(
+			"hpe_morpheus_form.example",
+			"option_type10_type",
+			"group",
+		),
 	}
 
 	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
