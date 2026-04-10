eb0a10ead45140850e935fe82caac99f4d5a728f
Jason Quigley <jasonq@mac.com>
2026-04-10 11:33:56 +0100

    add test for system image

--- morpheus/framework/datasources/ostypeimage/datasource_test.go
+++ morpheus/framework/datasources/ostypeimage/datasource_test.go
@@ -92,6 +92,66 @@ func TestAccMorpheusOsTypeImageDataSourceExampleOk(t *testing.T) {
 	})
 }
 
+func TestAccMorpheusOsTypeImageDataSourceSystemImageOk(t *testing.T) {
+	defer testhelpers.RecordResult(t)
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip("Skipping slow test in short mode")
+	}
+
+	testSystem := systemoverride.GetPreferred(t, "zodiac")
+	providerConfig := testhelpers.ProviderBlockForServer(testSystem)
+
+	dataSourceConfig, err := ostypeimage.RenderOsTypeImageDataSourceConfig(t,
+		map[string]string{
+			"OsTypeId":         "65",
+			"VirtualImageName": "Morpheus Debian 12 20260119",
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	checks := []resource.TestCheckFunc{
+		resource.TestCheckResourceAttrSet(
+			"data.hpe_morpheus_os_type_image.example",
+			"id",
+		),
+		resource.TestCheckResourceAttr(
+			"data.hpe_morpheus_os_type_image.example",
+			"tenant_id",
+			"0",
+		),
+		resource.TestCheckResourceAttr(
+			"data.hpe_morpheus_os_type_image.example",
+			"os_type_id",
+			"65",
+		),
+		resource.TestCheckResourceAttr(
+			"data.hpe_morpheus_os_type_image.example",
+			"virtual_image_name",
+			"Morpheus Debian 12 20260119",
+		),
+		resource.TestCheckResourceAttrSet(
+			"data.hpe_morpheus_os_type_image.example",
+			"virtual_image_id",
+		),
+	}
+
+	checkFn := resource.ComposeAggregateTestCheckFunc(checks...)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + dataSourceConfig,
+				Check:  checkFn,
+			},
+		},
+	})
+}
+
 func TestAccMorpheusOsTypeImageDataSourceNotFound(t *testing.T) {
 	defer testhelpers.RecordResult(t)
 	t.Parallel()
