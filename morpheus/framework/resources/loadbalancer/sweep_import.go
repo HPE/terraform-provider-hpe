b59d7fcb754850bd567e0c240d5a3bd17fff6518
RDavisHPE <135722327+RDavisHPE@users.noreply.github.com>
2026-04-22 16:52:49 +0100

    Refactoring Sweeping and Adding Cloud Sweeper (#1061)
    Refactoring sweeper to allow for self-registration (i.e. to add a
    sweeper - one just has to create a sweep.go file without modifying the
    centralized client in `sweep_test.go`) and use generics so that sweepers
    just have to provide a means of listing their respective resource,
    checking if a given resource is a test resource and deleting said
    resource (along with options such as filters and allowed API responses
    to maintain previous functionalities/checks).
    
    Also modified the Makefile to have an option to specify which resources
    one actually wants to sweep instead of having to append to the
    -sweep-run list (which was also a pain-point for adding new sweepers).
    ```
    make sweep SWEEP=hpe_morpheus_cloud
    ```
    
    ---------
    
    Co-authored-by: Ryan Davis <ryan.davis+hpeprod@hpe.com>

---
+++ morpheus/framework/resources/loadbalancer/sweep_import.go
@@ -0,0 +1,7 @@
+// (C) Copyright 2026 Hewlett Packard Enterprise Development LP
+
+//go:build sweep
+
+package loadbalancer
+
+import _ "github.com/HPE/terraform-provider-hpe/morpheus/framework/resources/loadbalancer/sweep"
---
+++ morpheus/framework/resources/policy/sweep/sweep.go
@@ -0,0 +1,65 @@
+// (C) Copyright 2025 Hewlett Packard Enterprise Development LP
+
+package sweep
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/HewlettPackard/hpe-morpheus-go-sdk/oapigen/sdk"
+
+	testsweep "github.com/HPE/terraform-provider-hpe/morpheus/testhelpers/sweep"
+)
+
+// Policies whose name begins with this string will be eligible for deletion
+const testPolicyPrefix = "TestAccMorpheusPolicy"
+
+func init() {
+	testsweep.RegisterTypedAPISweeper(
+		"hpe_morpheus_policy",
+		// List all policy resources.
+		func(ctx context.Context, client *sdk.APIClient) (
+			[]sdk.ListPolicies200ResponseAllOfPoliciesInner,
+			*http.Response,
+			error,
+		) {
+			resp, hresp, err := client.PoliciesAPI.ListPolicies(ctx).Execute()
+			if resp == nil {
+				return nil, hresp, err
+			}
+
+			return resp.GetPolicies(), hresp, err
+		},
+		// Is this a test policy?
+		func(item sdk.ListPolicies200ResponseAllOfPoliciesInner) bool {
+			name, ok := item.GetNameOk()
+			if !ok || name == nil {
+				return false
+			}
+
+			return strings.HasPrefix(*name, testPolicyPrefix)
+		},
+		// Delete the test policy.
+		func(
+			ctx context.Context,
+			client *sdk.APIClient,
+			item sdk.ListPolicies200ResponseAllOfPoliciesInner,
+		) (*http.Response, error) {
+			id, ok := item.GetIdOk()
+			if !ok || id == nil {
+				return nil, fmt.Errorf("could not get ID")
+			}
+
+			_, hresp, err := client.PoliciesAPI.RemovePolicies(ctx, *id).Execute()
+
+			return hresp, err
+		},
+		// Ignore (i.e. just log) "not found" and "forbidden" errors
+		testsweep.WithIgnoreListStatuses[sdk.ListPolicies200ResponseAllOfPoliciesInner](
+			http.StatusNotFound,
+			http.StatusForbidden,
+		),
+	)
+}
