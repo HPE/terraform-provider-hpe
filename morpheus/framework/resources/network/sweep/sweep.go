f41c61d8bb496bc0fd33a66f2b36537248e8b59b
RDavisHPE <135722327+RDavisHPE@users.noreply.github.com>
2026-04-23 11:50:44 +0100

    Simplifying sweeper code to just check for TestAccMorpheus prefix (#1071)
    Removing additional checks to sweepers and removing resources based on
    the TestAccMorpheus prefix to avoid dangling resources if
    - The resource doesn't have required tags
    - The resource has a slightly different prefix (i.e.
    TestAccMorpheusPolicyInstance for an instance that is created for a
    policy test).
    - The resource is a datastore (we previously skipped this entirely)
    
    ---------
    
    Co-authored-by: Ryan Davis <ryan.davis+hpeprod@hpe.com>

--- morpheus/framework/resources/network/sweep/sweep.go
+++ morpheus/framework/resources/network/sweep/sweep.go
@@ -13,36 +13,6 @@ import (
 	testsweep "github.com/HPE/terraform-provider-hpe/morpheus/testhelpers/sweep"
 )
 
-// Networks whose name begins with this string will be eligible for deletion
-const testNetworkPrefix = "TestAccMorpheusNetworkResource"
-
-// All of these labels must be present for the network to be deleted
-var requiredSweepLabels = []string{
-	"terraform",
-	"acctest",
-	"hpe_morpheus_network",
-	"sweepable",
-}
-
-func hasRequiredLabels(labels []string) bool {
-	if labels == nil {
-		return false
-	}
-
-	labelMap := make(map[string]bool)
-	for _, label := range labels {
-		labelMap[label] = true
-	}
-
-	for _, requiredLabel := range requiredSweepLabels {
-		if !labelMap[requiredLabel] {
-			return false
-		}
-	}
-
-	return true
-}
-
 func init() {
 	testsweep.RegisterTypedAPISweeper(
 		"hpe_morpheus_network",
@@ -62,16 +32,11 @@ func init() {
 		// Is this a test network?
 		func(item sdk.ListNetworks200ResponseAllOfNetworksInner) bool {
 			name, ok := item.GetNameOk()
-			if !ok || name == nil || !strings.HasPrefix(*name, testNetworkPrefix) {
+			if !ok || name == nil {
 				return false
 			}
 
-			labels, ok := item.GetLabelsOk()
-			if !ok || !hasRequiredLabels(labels) {
-				return false
-			}
-
-			return true
+			return strings.HasPrefix(*name, testsweep.TestResourcePrefix)
 		},
 		// Delete the test network.
 		func(
