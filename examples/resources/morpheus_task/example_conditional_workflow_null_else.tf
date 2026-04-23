75b2915bd9b9da93b455f7943dfb3bc2e3ca73f2
Eamonn O'Toole <eamonn.otoole@hpe.com>
2026-04-23 16:13:32 +0100

    Allow null else in conditional_workflow task
    We didn't support null else in conditional_workflows.  Thanks to Chris
    Taylor for uncovering the error and proposing a fix.  We've updated the
    docs and added acceptance tests.

---
+++ examples/resources/morpheus_task/example_conditional_workflow_null_else.tf
@@ -0,0 +1,22 @@
+resource "hpe_morpheus_task" "example_task" {
+  name           = "Example Conditional Workflow Task"
+  task_type_code = "conditionalWorkflow"
+  config_conditional_workflow = {
+    conditional_script           = <<-EOT
+        if (1 == true) {
+            return true;
+        }
+
+        return false;
+        EOT
+    if_operational_workflow_id   = "4090"
+    if_operational_workflow_name = "Example If Workflow"
+
+    else_operational_workflow_id   = null
+    else_operational_workflow_name = null
+  }
+
+  execute_target      = "local"
+  retryable           = false
+  allow_custom_config = true
+}
