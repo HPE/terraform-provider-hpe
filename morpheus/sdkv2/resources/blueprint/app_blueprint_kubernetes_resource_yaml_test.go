b008927eda431fe3ce69e24516aa65a4f3b47eec
Matthew Cunningham <156697246+matthew-c-hpe@users.noreply.github.com>
2026-04-23 18:16:38 +0100

    remove the skip from test

--- morpheus/sdkv2/resources/blueprint/app_blueprint_kubernetes_resource_yaml_test.go
+++ morpheus/sdkv2/resources/blueprint/app_blueprint_kubernetes_resource_yaml_test.go
@@ -23,8 +23,6 @@ func TestAccMorpheusAppBlueprintKubernetesYamlExampleOk(t *testing.T) {
 		t.Skip("Skipping slow test in short mode")
 	}
 
-	// t.Skip("Skipping due to missing infrastructure in test environment")
-
 	providerConfig := testhelpers.ProviderBlock()
 
 	name := acctest.RandomWithPrefix(t.Name())
