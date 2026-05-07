e66ecfc38c92ec71f69b91b006d7f07a840157f5
Jason Quigley <jasonq@mac.com>
2026-05-07 16:03:14 +0100

    change import separator to a dot

--- morpheus/framework/resources/networkrouterbgpneighbor/import.go
+++ morpheus/framework/resources/networkrouterbgpneighbor/import.go
@@ -16,12 +16,12 @@ func (r *Resource) ImportState(
 	req resource.ImportStateRequest,
 	resp *resource.ImportStateResponse,
 ) {
-	parts := strings.SplitN(req.ID, ":", 2)
+	parts := strings.SplitN(req.ID, ".", 2)
 	if len(parts) != 2 {
 		resp.Diagnostics.AddError(
 			"import network router bgp neighbor resource",
 			"provided import ID '"+req.ID+
-				"' is invalid, expected format 'router_id:id'",
+				"' is invalid, expected format 'router_id.id'",
 		)
 
 		return
