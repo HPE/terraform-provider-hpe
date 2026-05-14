22d4760ff60bfff1d236bb6fa7384d965c837ba4
Ryan Davis <ryan.davis+hpeprod@hpe.com>
2026-05-14 15:52:34 +0100

    Adding example

---
+++ examples/resources/morpheus_load_balancer/example_nsxt.tf
@@ -0,0 +1,12 @@
+resource "hpe_morpheus_load_balancer" "lb" {
+  name       = "example-terraform-nsxt-lb"
+  type_code  = "nsx-t"
+  visibility = "public"
+
+  config_nsxt = {
+    admin_state   = true
+    log_level     = "INFO"
+    size          = "SMALL"
+    tier1_gateway = "tier1-gateway"
+  }
+}
--- morpheus/framework/resources/loadbalancer/loadbalancer_example.go
+++ morpheus/framework/resources/loadbalancer/loadbalancer_example.go
@@ -13,6 +13,7 @@ import (
 
 //go:generate ../../../../bin/render -out examples/resources/morpheus_load_balancer/example_haproxy.tf example_haproxy.tf.tmpl Name "example-terraform-haproxy-lb" Description "HAProxy load balancer" CloudName "hvm" GroupName "Zodiac" PlanId "8" Pool "pool-574" Visibility "public"
 //go:generate ../../../../bin/render -out examples/resources/morpheus_load_balancer/example_haproxy_generic.tf example_haproxy_generic.tf.tmpl Name "example-terraform-haproxy-lb" Description "HAProxy load balancer via generic config" CloudName "hvm" GroupName "Zodiac" TypeCode "haproxyContainer" PlanId "8" Pool "pool-574" Visibility "public"
+//go:generate ../../../../bin/render -out examples/resources/morpheus_load_balancer/example_nsxt.tf example_nsxt.tf.tmpl Name "example-terraform-nsxt-lb" TypeCode "nsx-t" Visibility "public" AdminState "true" LogLevel "INFO" Size "SMALL" Tier1Gateway "tier1-gateway"
 
 func RenderLoadBalancerHAProxyConfig(t *testing.T, overrides map[string]string) (string, error) {
 	t.Helper()
@@ -93,39 +94,35 @@ func RenderLoadBalancerNsxtConfig(t *testing.T, overrides map[string]string) (st
 	t.Helper()
 
 	defaults := map[string]string{
-		"Name":       "example-terraform-nsxt-lb",
-		"TypeCode":   "nsx-t",
-		"Visibility": "public",
-		"AdminState": "true",
-		"Loglevel":   "INFO",
-		"Size":       "SMALL",
-		"Tier1":      "tier1-gateway",
+		"Name":         "example-terraform-nsxt-lb",
+		"TypeCode":     "nsx-t",
+		"Visibility":   "public",
+		"AdminState":   "true",
+		"LogLevel":     "INFO",
+		"Size":         "SMALL",
+		"Tier1Gateway": "tier1-gateway",
 	}
 
 	for key, value := range overrides {
 		defaults[key] = value
 	}
 
-	return fmt.Sprintf(`
-resource "hpe_morpheus_load_balancer" "lb" {
-  name       = %q
-  type_code  = %q
-  visibility = %q
+	var args []string
+	for key, value := range defaults {
+		args = append(args, key, value)
+	}
 
-  config_nsxt = {
-    admin_state         = %s
-    loglevel            = %q
-    size                = %q
-    tier1               = %q
-  }
-}
-`,
-		defaults["Name"],
-		defaults["TypeCode"],
-		defaults["Visibility"],
-		defaults["AdminState"],
-		defaults["Loglevel"],
-		defaults["Size"],
-		defaults["Tier1"],
-	), nil
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return "", fmt.Errorf("unable to get current file path")
+	}
+
+	dir := filepath.Dir(filename)
+	templatePath := filepath.Join(dir, "example_nsxt.tf.tmpl")
+
+	return testhelpers.RenderExample(
+		t,
+		templatePath,
+		args...,
+	)
 }
