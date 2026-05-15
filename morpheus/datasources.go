f2bf80010d07e34027efbb2ef7ddbf9ca1aaecc0
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2026-05-15 15:18:51 +0100

    merge dev-1.4 (#1129)
    Co-authored-by: Eamonn O'Toole <eamonn.otoole@hpe.com>
    Co-authored-by: rory-mcevoy-hpe <rory.mcevoy@hpe.com>
    Co-authored-by: Jason Quigley <jasonq@mac.com>
    Co-authored-by: matthew-c-hpe <156697246+matthew-c-hpe@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Signed-off-by: dependabot[bot] <support@github.com>

--- morpheus/datasources.go
+++ morpheus/datasources.go
@@ -21,6 +21,7 @@ import (
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/networkdhcpserver"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/networkdomain"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/networkrouter"
+	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/networkrouterbgpneighbor"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/ostype"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/ostypeimage"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/policy"
@@ -47,6 +48,7 @@ func (SubProvider) GetDataSources(
 		networkdhcpserver.NewDataSource,
 		networkdomain.NewDataSource,
 		networkrouter.NewDataSource,
+		networkrouterbgpneighbor.NewDataSource,
 		ostype.NewDataSource,
 		ostypeimage.NewDataSource,
 		policy.NewDataSource,
