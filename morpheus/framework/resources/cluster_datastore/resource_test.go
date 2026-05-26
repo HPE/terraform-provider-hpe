7a4d653cd75c3b9af6f2da328bf0cd40790b2e4d
Ryan Davis <ryan.davis+hpeprod@hpe.com>
2026-05-26 14:00:56 +0100

    Missing resource_test.go

---
+++ morpheus/framework/resources/cluster_datastore/resource_test.go
@@ -0,0 +1,18 @@
+// (C) Copyright 2026 Hewlett Packard Enterprise Development LP
+
+package cluster_datastore_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/HPE/terraform-provider-hpe/morpheus/testhelpers"
+	"github.com/HPE/terraform-provider-hpe/morpheus/testhelpers/systemoverride"
+)
+
+func TestMain(m *testing.M) {
+	systemoverride.ParseFlags()
+	code := testhelpers.TestMain(m)
+	testhelpers.WriteMergedResults()
+	os.Exit(code)
+}
