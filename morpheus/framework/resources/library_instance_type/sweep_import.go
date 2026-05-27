48106fea2b47c9f38dbd544b42513ae0806703dc
Ryan Davis <ryan.davis+hpeprod@hpe.com>
2026-05-27 16:57:53 +0100

    Removing library_instance_type

--- morpheus/framework/resources/library_instance_type/sweep_import.go
+++
@@ -1,9 +0,0 @@
-// (C) Copyright 2026 Hewlett Packard Enterprise Development LP
-
-//go:build sweep
-
-package library_instance_type
-
-import (
-	_ "github.com/HPE/terraform-provider-hpe/morpheus/framework/resources/library_instance_type/sweep"
-)
