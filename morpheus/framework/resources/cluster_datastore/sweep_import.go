23199c787083ec56c58d7c93230edfe26036715e
Ryan Davis <ryan.davis+hpeprod@hpe.com>
2026-05-27 17:02:43 +0100

    removing cluster_datastore

--- morpheus/framework/resources/cluster_datastore/sweep_import.go
+++
@@ -1,9 +0,0 @@
-// (C) Copyright 2026 Hewlett Packard Enterprise Development LP
-
-//go:build sweep
-
-package cluster_datastore
-
-import (
-	_ "github.com/HPE/terraform-provider-hpe/morpheus/framework/resources/cluster_datastore/sweep"
-)
