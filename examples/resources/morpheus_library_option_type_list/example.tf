2c1795a04f41fb73f1eef860ece275d8101ad6bd
Jason Quigley <jason.quigley@hpe.com>
2026-05-27 15:53:51 +0100

    Remove duplicate framework resources and rename option_list/container_script
    Remove 5 framework resources that duplicate existing SDKv2 resources:
    - cypher (duplicates cypher_secret + cypher_tfvars)
    - catalog_item_type (duplicates catalog_item_* resources + data source)
    - library_instance_type (duplicates instance_type)
    - library_spec_template (duplicates spec_template_*)
    - libraryfiletemplate (duplicates file_template)
    
    Rename 2 resources to shorter, clearer names:
    - library_option_type_list -> option_list (hpe_morpheus_option_list)
    - library_container_script -> container_script (hpe_morpheus_container_script)
    
    Also updates library_layout test to use sdkv2 hpe_morpheus_instance_type
    instead of the removed hpe_morpheus_library_instance_type.

--- examples/resources/morpheus_library_option_type_list/example.tf
+++
@@ -1,8 +0,0 @@
-resource "hpe_morpheus_library_option_type_list" "example" {
-  name        = "Region List"
-  description = "List of available regions"
-  type        = "rest"
-  source_url  = "https://api.example.com/regions"
-  visibility  = "public"
-  real_time   = false
-}
