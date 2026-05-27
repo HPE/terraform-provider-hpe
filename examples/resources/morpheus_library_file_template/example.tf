e54d08c00410b30033e0b6abd6886c212977bfaa
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2026-05-27 16:47:37 +0100

    Remove duplicate framework resources and rename option_list/container_script (#1153)

--- examples/resources/morpheus_library_file_template/example.tf
+++
@@ -1,7 +0,0 @@
-resource "hpe_morpheus_library_file_template" "example" {
-  name           = "Nginx Config"
-  file_name      = "nginx.conf"
-  file_path      = "/etc/nginx"
-  template_phase = "provision"
-  template       = file("/path-to-file")
-}
--- examples/resources/morpheus_library_spec_template/example.tf
+++
@@ -1,6 +0,0 @@
-resource "hpe_morpheus_library_spec_template" "example" {
-  name    = "Kubernetes Deployment"
-  type    = "kubernetes"
-  source  = "local"
-  content = file("/path-to-file")
-}
