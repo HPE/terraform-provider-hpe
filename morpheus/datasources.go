9f1d86e022e94a47503bb47297e4d0a6032fe153
Jason Quigley <138007593+jasonquigleyhpecom@users.noreply.github.com>
2026-06-25 17:10:17 +0100

    Revert "feat: Addition of security group datasources (#1197)"
    This reverts commit 181811977736b6b4f95656ba13e997875ff340fe.

--- morpheus/datasources.go
+++ morpheus/datasources.go
@@ -39,8 +39,6 @@ import (
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/ostypeimage"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/policy"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/role"
-	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/securitygroup"
-	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/securitygroups"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/serviceplan"
 	"github.com/HPE/terraform-provider-hpe/morpheus/framework/datasources/user"
 )
@@ -75,8 +73,6 @@ func (SubProvider) GetDataSources(
 		ostypeimage.NewDataSource,
 		policy.NewDataSource,
 		role.NewDataSource,
-		securitygroup.NewDataSource,
-		securitygroups.NewDataSource,
 		serviceplan.NewDataSource,
 		user.NewDataSource,
 		// hpegl VMaaS parity data sources
--- morpheus/framework/datasources/securitygroups/security_groups_example.go
+++
@@ -1,5 +0,0 @@
-// (C) Copyright 2026 Hewlett Packard Enterprise Development LP
-
-package securitygroups
-
-//go:generate ../../../../bin/render -out examples/data-sources/morpheus_security_groups/data-source.tf data-source.tf.tmpl
